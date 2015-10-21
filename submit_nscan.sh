d5a790d86d42873a45c682071c6d2867bce089eb
Greg Deakin <GregDeakin@users.noreply.github.com>
2015-10-21 10:53:01 +0100

    Update submit_nscan.sh

--- submit_nscan.sh
+++ submit_nscan.sh
@@ -3,9 +3,9 @@
 #$ -cwd
 #$ -l virtual_free=8G
 
-DATA=$1
-OUT=$2
-EVAL=$3
-HMM=$4
+DATA=$(sed -n -e "$SGE_TASK_ID p" split_files.txt)
+OUT=$DATA.$1
+EVAL=$2
+HMM=$3
 
 nhmmscan --noali --cpu 8 --incT 6 --tblout $OUT -E $EVAL $HMM $DATA >/dev/null
