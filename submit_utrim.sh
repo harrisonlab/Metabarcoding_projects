83fc2c49c088e8a17a1ae5c7cdbc1fec0191a0c3
Greg Deakin <GregDeakin@users.noreply.github.com>
2015-10-28 15:06:23 +0000

    Create submit_utrim.sh

---
+++ submit_utrim.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -l virtual_free=4G
+
+FASTQ=$1
+OUTFILE=$2
+OUTDIR=$3
+QUAL=$4
+MINL=$5
+
+
+mkdir -p $OUTDIR
+cd $OUTDIR
+
+usearch8 -fastq_filter $FASTQ -fastq_minlen $MINL -fastq_maxee_rate $QUAL -fastqout $OUTFILE
