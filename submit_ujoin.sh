dc29db53b51eb9cb99625155ff9cdc37dffd1b0d
Greg Deakin <GregDeakin@users.noreply.github.com>
2016-05-12 16:31:32 +0100

    Update submit_ujoin.sh

--- submit_ujoin.sh
+++ submit_ujoin.sh
@@ -7,12 +7,10 @@ F=$1
 R=$2
 OUTFILE=$3
 OUTDIR=$4
-QUAL=$5
-MINL=$6
 
 
 mkdir -p $OUTDIR
 cd $OUTDIR
 
-usearch8 -fastq_mergepairs $F -reverse $R -fastqout $OUTFILE -fastq_merge_maxee $QUAL -fastq_minlen $MINL
+usearch8 -fastq_mergepairs $F -reverse $R -fastqout $OUTFILE
 
