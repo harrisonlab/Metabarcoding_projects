137b572d7ec6ac6767ff0675901f8c9c048e3a6d
Greg Deakin <GregDeakin@users.noreply.github.com>
2016-06-21 12:01:51 +0100

    Add files via upload

--- submit_16Spre.sh
+++ submit_16Spre.sh
@@ -10,17 +10,24 @@ OUTDIR=$4
 ADAPTERS=$5
 MINL=$6
 QUAL=$7
-LABEL=$8
+SCRIPT_DIR=$8
+
+LABEL=${OUTFILE}.
 
 mkdir -p $OUTDIR 
 cd $OUTDIR 
 
 usearch8.1 -fastq_mergepairs $F -reverse $R -fastqout ${OUTFILE}.t1 
 usearch8.1 -search_oligodb ${OUTFILE}.t1 -db $ADAPTERS -strand both -userout ${OUTFILE}.t1.txt -userfields query+target+qstrand+diffs+tlo+thi+trowdots 
-cat ${OUTFILE}.t1.txt|awk -F"\t" '{print $1}'|sort|uniq|xargs -I Â¬ sed -i -ne:t -e"/*\@Â¬.*/D" -e'$!N;//D;/'"\@Â¬/{" -e"s/\n/&/3;t" -e'$q;bt' -e\} -e's/\n/&/'"1;tP" -e'$!bt' -e:P  -e'P;D' ${OUTFILE}.t1
-usearch8.1 -fastq_filter ${OUTFILE}.t1 -fastq_minlen $MINL -fastq_maxee_rate $QUAL -relabel $LABEL -fastaout $OUTFILE
+
+
+cat ${OUTFILE}.t1.txt|awk -F"\t" '{print $1}'|sort|uniq|$SCRIPT_DIR/adapt_delete.pl ${OUTFILE}.t1 > ${OUTFILE}.t2
+
+#xargs -I ¬ sed -i -ne:t -e"/*\@¬.*/D" -e'$!N;//D;/'"\@¬/{" -e"s/\n/&/3;t" -e'$q;bt' -e\} -e's/\n/&/'"1;tP" -e'$!bt' -e:P  -e'P;D' ${OUTFILE}.t1
+
+usearch8.1 -fastq_filter ${OUTFILE}.t2 -fastq_minlen $MINL -fastq_maxee_rate $QUAL -relabel $LABEL -fastaout ${OUTFILE}.filtered.fa
 
 mkdir -p $OUTDIR/../unfiltered 
-mv ${OUTFILE}.t1 $OUTDIR/../unfiltered/${OUTFILE}.unfiltered.fastq
+mv ${OUTFILE}.t2 $OUTDIR/../unfiltered/${OUTFILE}.unfiltered.fastq
 
-rm ${OUTFILE}.t1.txt
+rm ${OUTFILE}.t1.txt ${OUTFILE}.t1
--- submit_ITSpre.sh
+++ submit_ITSpre.sh
@@ -22,15 +22,13 @@ usearch8.1 -search_oligodb $F -db $PRIMERS -strand both -userout ${F}.t1.txt -us
 usearch8.1 -search_oligodb $R -db $PRIMERS -strand both -userout ${R}.t1.txt -userfields query+target+qstrand+diffs+tlo+thi+trowdots 
 
 sed 's|^|/|;s|$|/,+3 d|' <(grep primer1 ${F}.t1.txt|awk -F"\t" '{print $1}') > ${OUTFILE}.temp.sed
-sed -f ${OUTFILE}.temp.sed $F > ${F}.t.fastq
+sed -f ${OUTFILE}.temp.sed $F > ${F}.t1.fastq
 sed 's|^|/|;s|$|/,+3 d|' <(grep primer2 ${R}.t1.txt|awk -F"\t" '{print $1}') > ${OUTFILE}.temp.sed
-sed -f ${OUTFILE}.temp.sed $F > ${R}.t.fastq 
+sed -f ${OUTFILE}.temp.sed $F > ${R}.t1.fastq 
 
-grep adapter ${F}.t1.txt|awk -F"\t" '{print $1}'|sort|uniq|$SCRIPT_DIR/adapt_delete.pl ${F}.t.fastq > ${F}.t2.fastq
-grep adapter ${R}.t1.txt|awk -F"\t" '{print $1}'|sort|uniq|$SCRIPT_DIR/adapt_delete.pl ${R}.t.fastq > ${R}.t2.fastq
+grep adapter ${F}.t1.txt|awk -F"\t" '{print $1}'|sort|uniq|$SCRIPT_DIR/adapt_delete.pl ${F}.t1.fastq > ${F}.t2.fastq
+grep adapter ${R}.t1.txt|awk -F"\t" '{print $1}'|sort|uniq|$SCRIPT_DIR/adapt_delete.pl ${R}.t1.fastq > ${R}.t2.fastq
 
-#grep adapter ${F}.t1.txt|awk -F"\t" '{print $1}'|sort|uniq|xargs -I Â¬ sed -i -ne:t -e"/*\@Â¬.*/D" -e'$!N;//D;/'"\@Â¬/{" -e"s/\n/&/3;t" -e'$q;bt' -e\} -e's/\n/&/'"1;tP" -e'$!bt' -e:P  -e'P;D' ${F}.t.fastq
-#grep adapter ${R}.t1.txt|awk -F"\t" '{print $1}'|sort|uniq|xargs -I Â¬ sed -i -ne:t -e"/*\@Â¬.*/D" -e'$!N;//D;/'"\@Â¬/{" -e"s/\n/&/3;t" -e'$q;bt' -e\} -e's/\n/&/'"1;tP" -e'$!bt' -e:P  -e'P;D' ${R}.t.fastq
 
 usearch8.1 -fastq_filter ${F}.t2.fastq -fastq_minlen $MINL -fastq_maxee_rate $QUALF -relabel $LABEL -fastaout ${OUTFILE}_t1.fa
 usearch8.1 -fastq_filter ${R}.t2.fastq -fastq_minlen $MINL -fastq_maxee_rate $QUALR -relabel $LABEL -fastaout ${OUTFILE}_t2.fa
@@ -41,4 +39,13 @@ sed -i -e '1d' ${OUTFILE}_R1.fa
 awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}'  <${OUTFILE}_t2.fa > ${OUTFILE}_R2.fa
 sed -i -e '1d' ${OUTFILE}_R2.fa
 
-rm ${F}.t1.txt ${R}.t1.txt ${F}.t.fastq ${R}.t.fastq ${F}.t2.fastq ${R}.t2.fastq ${OUTFILE}_t1.fa ${OUTFILE}_t2.fa ${OUTFILE}.temp.sed
+mkdir -p $OUTDIR/../unfiltered 
+mv ${F}.t2.fastq $OUTDIR/../unfiltered/${OUTFILE}.r1.unfiltered.fastq
+mv ${R}.t2.fastq $OUTDIR/../unfiltered/${OUTFILE}.r2.unfiltered.fastq
+
+
+rm ${F}.t1.txt ${R}.t1.txt ${F}.t1.fastq ${R}.t1.fastq ${OUTFILE}_t1.fa ${OUTFILE}_t2.fa ${OUTFILE}.temp.sed
+
+
+#xargs -I ¬ sed -i -ne:t -e"/*\@¬.*/D" -e'$!N;//D;/'"\@¬/{" -e"s/\n/&/3;t" -e'$q;bt' -e\} -e's/\n/&/'"1;tP" -e'$!bt' -e:P  -e'P;D' ${F}.t.fastq
+#xargs -I ¬ sed -i -ne:t -e"/*\@¬.*/D" -e'$!N;//D;/'"\@¬/{" -e"s/\n/&/3;t" -e'$q;bt' -e\} -e's/\n/&/'"1;tP" -e'$!bt' -e:P  -e'P;D' ${R}.t.fastq
