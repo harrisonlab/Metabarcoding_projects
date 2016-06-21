01dad7cd9e674d4f1ecd88be59ff4c7f5e318f23
Greg Deakin <GregDeakin@users.noreply.github.com>
2016-06-21 12:06:06 +0100

    Update adapt_delete.pl

--- adapt_delete.pl
+++ adapt_delete.pl
@@ -1,8 +1,13 @@
 #!/usr/bin/perl -w -s
 
+##########################################################################################################
+# Input is stdin for list of FASTQ read headers containing adapters and command line arg for FASTQ file  #
+# Output is FASTQ excluding reads from stdin								 #
+##########################################################################################################
+
+# slurp stdin
 my $headers = do { local $/; <STDIN> };
 
-#$headers =~ s/\R//g;
 my ($inpFile) = $ARGV[0] || die "Please provide fastq file" ;
 unless(open(INFILE, $inpFile) ) {
 	print("Cannot open input file \"$inpFile\"\n\n");
