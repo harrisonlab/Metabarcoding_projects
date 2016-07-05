8dcd777027a083fb6ffef8ba747c4fbcf86a52ef
Greg Deakin <GregDeakin@users.noreply.github.com>
2016-07-05 11:52:57 +0100

    Update submit_16Spre.sh

--- submit_16Spre.sh
+++ submit_16Spre.sh
@@ -23,7 +23,7 @@ usearch8.1 -search_oligodb ${OUTFILE}.t1 -db $ADAPTERS -strand both -userout ${O
 
 cat ${OUTFILE}.t1.txt|awk -F"\t" '{print $1}'|sort|uniq|$SCRIPT_DIR/adapt_delete.pl ${OUTFILE}.t1 > ${OUTFILE}.t2
 
-#xargs -I ¬ sed -i -ne:t -e"/*\@¬.*/D" -e'$!N;//D;/'"\@¬/{" -e"s/\n/&/3;t" -e'$q;bt' -e\} -e's/\n/&/'"1;tP" -e'$!bt' -e:P  -e'P;D' ${OUTFILE}.t1
+#xargs -I Â¬ sed -i -ne:t -e"/*\@Â¬.*/D" -e'$!N;//D;/'"\@Â¬/{" -e"s/\n/&/3;t" -e'$q;bt' -e\} -e's/\n/&/'"1;tP" -e'$!bt' -e:P  -e'P;D' ${OUTFILE}.t1
 
 usearch8.1 -fastq_filter ${OUTFILE}.t2 -fastq_minlen $MINL -fastq_maxee $QUAL -relabel $LABEL -fastaout ${OUTFILE}.t3.fa
 
@@ -32,6 +32,6 @@ sed -i -e '1d' ${OUTFILE}.filtered.fa
 
 
 mkdir -p $OUTDIR/../unfiltered 
-mv ${OUTFILE}.t2 ${OUTFILE}.t2 $OUTDIR/../unfiltered/${OUTFILE}.unfiltered.fastq
+mv ${OUTFILE}.t2 $OUTDIR/../unfiltered/${OUTFILE}.unfiltered.fastq
 
-rm ${OUTFILE}.t1.txt ${OUTFILE}.t1 ${OUTFILE}.t3
+rm ${OUTFILE}.t1.txt ${OUTFILE}.t1 ${OUTFILE}.t3.fa
