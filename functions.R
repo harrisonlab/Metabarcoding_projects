435ac77847f51794e3f53fafe14336dbbfb20899
Greg Deakin <GregDeakin@users.noreply.github.com>
2016-08-16 10:08:53 +0100

    Update functions.R

--- functions.R
+++ functions.R
@@ -1,28 +1,68 @@
-ubiom <- function(locX,locY,locZ) {
-	options(stringsAsFactors = FALSE)
-	countData <- read.table(locX,header=T,sep="\t", comment.char="")
-	rownames(countData ) <- countData [,1]
-	countData <- countData [,-1]
-	taxa <- read.csv(locY,header=F)
-	taxa <- taxa [,c(1,2,4,6,8,10,12,14)]
-	rownames(taxa) <- taxa[,1]
-	taxa <- taxa[,-1]
-	colnames(taxa) <- c("kingdom", "phylum", "class", "order", "family", "genus", "species")
-	colData <- read.table(locZ,sep="\t",header=T)
-	rownames(colData) <- colData [,1]
-	colData <- colData[,-1,drop=FALSE]
-	countData <- countData[,rownames(colData)]
-	ls.biom <- list(countData,colData, taxa)
-	names(ls.biom) <- c("countData","colData","taxa")
-	return(ls.biom)
-}
-
 combine_biom <- function(locX,locY) {
 	biom1 <- read.table(locX,header=T,sep="\t", comment.char="")	
 	biom2 <- read.table(locY,header=T,sep="\t", comment.char="")
 	biom <- merge(biom1,biom2,by.x="X.OTU.ID",by.y="X.OTU.ID",all=T)
 	biom[is.na(biom)] <- 0
 	return(biom)	
+}
+
+ddsCalc <- function(X, design=~condition) {
+	suppressPackageStartupMessages(require(DESeq2))
+	dds <- 	DESeqDataSetFromMatrix(X$countData,X$colData,design)
+	if (sum(apply(X$countData,1,function(x) prod(x!=0)))>0) {
+		suppressPackageStartupMessages(require(edgeR))
+		sizeFactors(dds) <- calcNormFactors(counts(dds))	
+	} else {
+		print("every gene contains at least one zero")
+		print("ignoring all zero values")
+		gm_mean = function(x, na.rm=TRUE){
+			exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+		}
+		geoMeans = apply(counts(dds), 1, gm_mean)
+		dds <- estimateSizeFactors(dds, geoMeans = geoMeans)
+	}
+	return(DESeq(dds, fitType="local"))
+} 
+
+fltTaxon <- function(X,taxon="phylum") {
+  n <- which(colnames(X$taxa)==taxon)
+	x <- aggregate(X$countData,by=X$taxa[,1:n],sum)
+	ls.biom <- list(x[,(n+1):ncol(x)],X$colData,x[,1:n])
+	names(ls.biom) <- c("countData","colData","taxa")
+	return(ls.biom)	
+}
+
+plotPCA <- function (object, intgroup = "condition", ntop = 500,pcx = 1,pcy = 2, returnData = FALSE)
+{
+    suppressPackageStartupMessages(require(genefilter))
+    suppressPackageStartupMessages(require(ggplot2))
+    rv <- rowVars(assay(object))
+    select <- order(rv, decreasing = TRUE)[seq_len(min(ntop,
+        length(rv)))]
+    pca <- prcomp(t(assay(object)[select, ]))
+    percentVar <- pca$sdev^2/sum(pca$sdev^2)
+    if (!all(intgroup %in% names(object@colData))) {
+        stop("the argument 'intgroup' should specify columns of colData")
+    }
+    intgroup.df <- as.data.frame(object@colData[, intgroup,
+        drop = FALSE])
+    group <- if (length(intgroup) > 1) {
+        factor(apply(intgroup.df, 1, paste, collapse = " : "))
+    }
+    else {
+        colData(object)[[intgroup]]
+    }
+    d <- data.frame(PC1 = pca$x[, pcx], PC2 = pca$x[, pcy], group = group,
+        intgroup.df, name = object$label)
+    if (returnData) {
+        attr(d, "percentVar") <- percentVar[1:2]
+        return(d)
+    }
+
+    ggplot() +
+    geom_point(data=d, mapping=aes(x=PC1, y=PC2, colour=group),size=3) +
+    xlab(paste0("PC",pcx,": ", round(percentVar[pcx] * 100), "% variance")) +
+    ylab(paste0("PC",pcy,": ", round(percentVar[pcy] * 100), "% variance"))
 }
 
 plotPCAWithLabels <- function (object, intgroup = "condition", ntop = 500,pcx = 1,pcy = 2, returnData = FALSE)
@@ -59,55 +99,22 @@ plotPCAWithLabels <- function (object, intgroup = "condition", ntop = 500,pcx = 
     ylab(paste0("PC",pcy,": ", round(percentVar[pcy] * 100), "% variance"))
 }
 
-plotPCA <- function (object, intgroup = "condition", ntop = 500,pcx = 1,pcy = 2, returnData = FALSE)
-{
-    suppressPackageStartupMessages(require(genefilter))
-    suppressPackageStartupMessages(require(ggplot2))
-    rv <- rowVars(assay(object))
-    select <- order(rv, decreasing = TRUE)[seq_len(min(ntop,
-        length(rv)))]
-    pca <- prcomp(t(assay(object)[select, ]))
-    percentVar <- pca$sdev^2/sum(pca$sdev^2)
-    if (!all(intgroup %in% names(object@colData))) {
-        stop("the argument 'intgroup' should specify columns of colData")
+phylo_to_des <- function(X, design=~condition) {
+    suppressPackageStartupMessages(require(DESeq2))
+    dds <-  phyloseq_to_deseq2(X,design)
+    if (sum(apply(counts(dds),1,function(x) prod(x!=0)))>0) {
+        suppressPackageStartupMessages(require(edgeR))
+        sizeFactors(dds) <- calcNormFactors(counts(dds))    
+    } else {
+        print("every gene contains at least one zero")
+        print("ignoring all zero values")
+        gm_mean = function(x, na.rm=TRUE){
+            exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+        }
+        geoMeans = apply(counts(dds), 1, gm_mean)
+        dds <- estimateSizeFactors(dds, geoMeans = geoMeans)
     }
-    intgroup.df <- as.data.frame(object@colData[, intgroup,
-        drop = FALSE])
-    group <- if (length(intgroup) > 1) {
-        factor(apply(intgroup.df, 1, paste, collapse = " : "))
-    }
-    else {
-        colData(object)[[intgroup]]
-    }
-    d <- data.frame(PC1 = pca$x[, pcx], PC2 = pca$x[, pcy], group = group,
-        intgroup.df, name = object$label)
-    if (returnData) {
-        attr(d, "percentVar") <- percentVar[1:2]
-        return(d)
-    }
-
-    ggplot() +
-    geom_point(data=d, mapping=aes(x=PC1, y=PC2, colour=group),size=3) +
-    xlab(paste0("PC",pcx,": ", round(percentVar[pcx] * 100), "% variance")) +
-    ylab(paste0("PC",pcy,": ", round(percentVar[pcy] * 100), "% variance"))
-}
-
-ddsCalc <- function(X, design=~condition) {
-	suppressPackageStartupMessages(require(DESeq2))
-	dds <- 	DESeqDataSetFromMatrix(X$countData,X$colData,design)
-	if (sum(apply(X$countData,1,function(x) prod(x!=0)))>0) {
-		suppressPackageStartupMessages(require(edgeR))
-		sizeFactors(dds) <- calcNormFactors(counts(dds))	
-	} else {
-		print("every gene contains at least one zero")
-		print("ignoring all zero values")
-		gm_mean = function(x, na.rm=TRUE){
-			exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
-		}
-		geoMeans = apply(counts(dds), 1, gm_mean)
-		dds <- estimateSizeFactors(dds, geoMeans = geoMeans)
-	}
-	return(DESeq(dds, fitType="local"))
+    return(DESeq(dds, fitType="local"))
 } 
 
 sumTaxa <- function(X,taxon="phylum",condition="condition") {
@@ -133,10 +140,22 @@ sumTaxa <- function(X,taxon="phylum",condition="condition") {
 	return(nd)
 }
 
-fltTaxon <- function(X,taxon="phylum") {
-  n <- which(colnames(X$taxa)==taxon)
-	x <- aggregate(X$countData,by=X$taxa[,1:n],sum)
-	ls.biom <- list(x[,(n+1):ncol(x)],X$colData,x[,1:n])
+ubiom <- function(locX,locY,locZ) {
+	options(stringsAsFactors = FALSE)
+	countData <- read.table(locX,header=T,sep="\t", comment.char="")
+	rownames(countData ) <- countData [,1]
+	countData <- countData [,-1]
+	taxa <- read.csv(locY,header=F)
+	taxa <- taxa [,c(1,2,4,6,8,10,12,14)]
+	rownames(taxa) <- taxa[,1]
+	taxa <- taxa[,-1]
+	colnames(taxa) <- c("kingdom", "phylum", "class", "order", "family", "genus", "species")
+	colData <- read.table(locZ,sep="\t",header=T)
+	rownames(colData) <- colData [,1]
+	colData <- colData[,-1,drop=FALSE]
+	countData <- countData[,rownames(colData)]
+	ls.biom <- list(countData,colData, taxa)
 	names(ls.biom) <- c("countData","colData","taxa")
-	return(ls.biom)	
+	return(ls.biom)
 }
+
