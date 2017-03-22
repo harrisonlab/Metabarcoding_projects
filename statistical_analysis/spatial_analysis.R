446dde0503dafc4a29e0a5b992e4c93b014b1379
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-03-22 16:54:41 +0000

    Update spatial_analysis.R

--- statistical_analysis/spatial_analysis.R
+++ statistical_analysis/spatial_analysis.R
@@ -1,9 +1,10 @@
 library(DESeq2)
 library(phyloseq)
-library(ape)
-library(vegan)
-library(ncf)
+#library(ape)
+#library(vegan)
+#library(ncf)
 library(data.table)
+library(gridExtra)
 library(devtools)
 load_all("../..//metabarcoding_pipeline/scripts/myfunctions")
 
@@ -80,6 +81,55 @@ mygplots <- lapply(seq(1,2),function(x)
   }    
 )
 
+mylegend <- ggplot_legend(mygplots[[1]][[1]]+theme(legend.direction="horizontal",legend.box="horizontal"))
+mylegend$layout$clip[mylegend$layout$name == "panel"] <- "off"  
+  
+mx <- mygplots
+counter <- 0
+mx <- lapply(mx,function(x) 
+  lapply(x,function(y) {
+    counter <<- counter + 1
+    y <- y+theme(legend.position="none",
+                 axis.line.x = element_line(size=0.5,colour = "black"),
+                 axis.line.y = element_line(size=0.5,colour = "black"),
+                 axis.text = element_text(colour = "black"))+ 
+    geom_text(aes(label = LETTERS[counter], x = 5, y = 7), hjust = 1,color="black")
+    #y <- ggplot_gtable(ggplot_build(y))
+    #y$layout$clip[y$layout$name == "panel"] <- "off"
+    return(y)
+  }
+))  
+  
+pdf("test.pdf",width=8,height=6)
+grid.arrange(
+  mx[[1]][1],
+  mx[[2]][1],
+  mx[[1]][2],
+  mx[[2]][2],
+  mylegend,
+ncol=2,nrow=3),
+layout_matrix = rbind(c(1,2),c(3,4),c(5,5)),widths = c(2.7, 2.7), heights = c(2.5,2.5, 0.2)            
+)  
+dev.off()
+
+grid.arrange(
+mygplots[[1]][[1]] + theme(legend.position="none"),
+mygplots[[1]][[1]] + theme(legend.position="none"),
+mygplots[[1]][[1]] + theme(legend.position="none"),
+mygplots[[1]][[1]] + theme(legend.position="none"),
+mylegend, nrow=3,ncol=2,
+layout_matrix = rbind(c(1,2),c(3,4),c(5,5)),widths = c(2.7, 2.7), heights = c(2.5,2.5, 0.2)
+)  
+mylegend<-g_legend(g5)
+mx[[5]]<-mylegend
+
+pdf("dge_reordered.pdf",height=10,width=9)
+# grid.arrange(g1,g2,g3,g4,mylegend,ncol=2,nrow=3,layout_matrix = rbind(c(1,2),c(3,4), c(5,5)),widths = c(2.7, 2.7), heights = c(2.5,2.5, 0.2))
+
+grid.arrange(grobs=mx,ncol=2,nrow=3,layout_matrix = rbind(c(1,2),c(3,4), c(5,5)),widths = c(2.7, 2.7), heights = c(2.5,2.5, 0.2))
+dev.off()
+
+  
 #Manova/Anova of first couple of pca with tree/aisle
 #fit <- manova(mypca$x[,1:3]~condition,as.data.frame(as.matrix(sample_data(myfiltbiom))))
 #summary(fit, test="Pillai") # could just call summary directly 
