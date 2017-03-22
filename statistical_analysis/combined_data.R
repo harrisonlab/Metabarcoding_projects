122af5f69eb4bffd320890845507b3ff946977af
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-03-22 10:58:37 +0000

    Update combined_data.R

--- statistical_analysis/combined_data.R
+++ statistical_analysis/combined_data.R
@@ -1,4 +1,11 @@
-
+library(DESeq2)
+library(phyloseq)
+library(ape)
+library(vegan)
+library(ncf)
+library(data.table)
+library(devtools)
+load_all("../..//metabarcoding_pipeline/scripts/myfunctions")
 
 myfiltbiom <- prune_samples(sample_data(mybiom)[[10]]!="duplicate",mybiom)
 myfiltbiom@sam_data$location <- as.factor(myfiltbiom@sam_data$meters)
@@ -6,9 +13,13 @@ myfiltbiom@sam_data$location <- as.factor(myfiltbiom@sam_data$meters)
 tempiom <- myfiltbiom
 tempiom@otu_table@.Data <-  assay(varianceStabilizingTransformation(phylo_to_des(tempiom)))
 
+filterFun=function(o,f){
+  prune_samples(sample_data(o)[[11]]==f&sample_data(o)[[1]]!="C",o)
+}
+
 mypca <- list(
-  dessert=plotPCA(tempiom,returnData=T,trans=F,filterFun=function(o,f){prune_samples(sample_data(o)[[11]]=="Dessert"&sample_data(o)[[1]]!="C",o)}),
-  cider=plotPCA(tempiom,returnData=T,trans=F,filterFun=function(o,f){prune_samples(sample_data(o)[[11]]=="Cider"&sample_data(o)[[1]]!="C",o)})
+  dessert=plotPCA(tempiom,returnData=T,trans=F,filterFun=filterFun,filter="Dessert"),
+  cider=plotPCA(tempiom,returnData=T,trans=F,filterFun=filterFun,filter="Cider")
 )
 rm(tempiom)
 
@@ -16,12 +27,18 @@ myfiltbiom <- prune_samples(sample_data(myfiltbiom)[[1]]!="C",myfiltbiom)
 myfiltbiom <- prune_taxa(rowSums(otu_table(myfiltbiom))>5,myfiltbiom)
 
 myfiltbiom <- list( 
-  dessert=prune_samples(sample_data(myfiltbiom)[[11]]=="Dessert",myfiltbiom),
-  cider=prune_samples(sample_data(myfiltbiom)[[11]]=="Cider",myfiltbiom)
+  dessert=filterFun(myfiltbiom,"Dessert"),
+  cider=filterFun(myfiltbiom,"Cider")
 )
 
 # get the sum of squares for tree/aisle, location and residual
-sum_squares <- t(apply(mypca$x,2,function(x) t(summary(aov(x~condition+location,sample_data(myfiltbiom)))[[1]][2])))
+sum_squares <- list(
+  dessert=(apply(mypca$dessert$x,2,function(x) 
+    t(summary(aov(x~condition+location,data.frame(as.matrix(sample_data(myfiltbiom$dessert)))))[[1]][2]))),
+  cider=(apply(mypca$cider$x,2,function(x) 
+    t(summary(aov(x~condition+location,data.frame(as.matrix(sample_data(myfiltbiom$cider)))))[[1]][2])))
+)
+    
 colnames(sum_squares) <- c("condition","location","residual")
 perVar <- sum_squares * mypca$percentVar
 colSums(perVar)
