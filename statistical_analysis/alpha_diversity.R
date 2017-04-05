6e1934083419f696163782132a434ff66b3970ee
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-04-05 17:00:18 +0100

    Update alpha_diversity.R

--- statistical_analysis/alpha_diversity.R
+++ statistical_analysis/alpha_diversity.R
@@ -1,6 +1,6 @@
 # filter out duplicates and control samples
 myfiltbiom <- prune_samples(sample_data(mybiom)[[10]]!="duplicate",mybiom)
-colnames(sample_data(myfiltbiom))[c(1,6)] <- c("Sample","Distance")
+colnames(sample_data(myfiltbiom))[c(1,6,11)] <- c("Sample","Distance","Orchard")
 levels(sample_data(myfiltbiom)[[1]]) <- c("C","Aisle","Tree")
 myfiltbiom<-prune_samples(sample_data(myfiltbiom)[[1]]!="C",myfiltbiom)
 
@@ -8,12 +8,12 @@ myfiltbiom<-prune_samples(sample_data(myfiltbiom)[[1]]!="C",myfiltbiom)
 all_alpha <- plot_richness(myfiltbiom,returnData=T)
 
 # ANOVA of alpha data
-summary(aov(Chao1~(Sample*orchard)+(location*orchard),all_alpha))[[1]][2]
-summary(aov(Shannon~(Sample*orchard)+(location*orchard),all_alpha))[[1]][2]
-summary(aov(Simpson~(Sample*orchard)+(location*orchard),all_alpha))[[1]][2]
+summary(aov(Chao1~location+(Sample*Orchard),all_alpha))[[1]][2]
+summary(aov(Shannon~location+(Sample*Orchard)),all_alpha))[[1]][2]
+summary(aov(Simpson~location+(Sample*Orchard),all_alpha))[[1]][2]
 #~condition*orchard+location
 
-sample_data(myfiltbiom)$Class <- paste(sample_data(myfiltbiom)$orchard,sample_data(myfiltbiom)$Sample,sep=" ")
+sample_data(myfiltbiom)$Class <- paste(sample_data(myfiltbiom)$Orchard,sample_data(myfiltbiom)$Sample,sep=" ")
 
 pdf("Alpha.pdf", height=8,width=8)
 plot_richness(myfiltbiom,x="Class",color="Distance",measures=c("Chao1", "Shannon", "Simpson"))
