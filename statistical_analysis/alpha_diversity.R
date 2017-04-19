e8cce9c3034548f0319d77cf959e5fa264270756
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-04-19 16:32:49 +0100

    Update alpha_diversity.R

--- statistical_analysis/alpha_diversity.R
+++ statistical_analysis/alpha_diversity.R
@@ -13,6 +13,11 @@ colnames(sample_data(myfiltbiom))[c(1,6,11)] <- c("Sample","Distance","Orchard")
 levels(sample_data(myfiltbiom)[[1]]) <- c("C","Aisle","Tree")
 myfiltbiom<-prune_samples(sample_data(myfiltbiom)[[1]]!="C",myfiltbiom)
 sample_data(myfiltbiom)$Class <- paste(sample_data(myfiltbiom)$Orchard,sample_data(myfiltbiom)$Sample,sep=" ")
+sample_data(myfiltbiom)$Class[sample_data(myfiltbiom)$Class=="Cider Aisle"] <- "C-A"
+sample_data(myfiltbiom)$Class[sample_data(myfiltbiom)$Class=="Cider Tree"] <- "C-T"
+sample_data(myfiltbiom)$Class[sample_data(myfiltbiom)$Class=="Dessert Aisle"] <- "D-A"
+sample_data(myfiltbiom)$Class[sample_data(myfiltbiom)$Class=="Dessert Tree"] <- "D-T"
+
 
 # Get alpha data
 all_alpha <- plot_richness(myfiltbiom,returnData=T)
@@ -33,12 +38,14 @@ summary(aov(Simpson~location+(Sample*Orchard),all_alpha))[[1]][2]
 g1 <- plot_richness(myfiltbiom,x="Class",color="Distance",measures=c("Chao1", "Shannon", "Simpson"))
 g2 <- plot_richness(myfiltbiom,x="Class",color="Distance",measures=c("Chao1", "Shannon", "Simpson"))
 
+title.A <- textGrob(label = "A",x = unit(0, "lines"),y = unit(0, "lines"),hjust = -0.5, vjust = 0,gp = gpar(fontsize = 16))
+title.B <- textGrob(label = "B",x = unit(0, "lines"),y = unit(0, "lines"),hjust = -0.5, vjust = -1,gp = gpar(fontsize = 16))
+
 #dev.off()
 g1 <- g1 + theme(legend.position="none",
                  axis.title.x=element_blank(),
                  axis.title.y=element_blank(),
-                 plot.margin=unit(c(1,1,1.5,0.5), "lines"),
-                 plot.title = element_text(hjust=-0.1)
+                 plot.margin=unit(c(-1,1,2.5,0.5), "lines")
                 )
 g2 <- g2 + theme(legend.direction="horizontal",
                  legend.position="bottom",
@@ -47,29 +54,12 @@ g2 <- g2 + theme(legend.direction="horizontal",
                  legend.box.just="left",
                  axis.title.x=element_blank(),
                  axis.title.y=element_blank(),
-                 plot.margin=unit(c(-1.0,1,0.5,0.5), "lines"),
-                 plot.title = element_text(hjust=-2)
+                 plot.margin=unit(c(-2,1,0.5,0.5), "lines")
                 )
-
-title.A <- textGrob(
-    label = "A",
-    x = unit(0, "lines"), 
-    y = unit(0, "lines"),
-    hjust = 0, vjust = 0,
-    gp = gpar(fontsize = 16))
-
-title.B <- textGrob(
-    label = "B",
-    x = unit(0, "lines"), 
-    y = unit(0, "lines"),
-    hjust = 0, vjust = 0,
-    gp = gpar(fontsize = 16))
-
-p1 <- arrangeGrob(p, top = title.grob)
-grid.arrange(p1)
-
+g3 <- arrangeGrob(g1, top = title.A)
+g4 <- arrangeGrob(g2, top = title.B)
 
 pdf("Alpha.pdf", height=8,width=8)
 lay=rbind(c(1,1),c(2,2))
-grid.arrange(g1,g2,layout_matrix=lay)
+grid.arrange(g3,g4,layout_matrix=lay)
 dev.off()
