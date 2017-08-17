aaac134205d7bdc7d81e0b307f46cf9e10c45547
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-08-17 11:46:41 +0100

    Update analysis.R

--- Bean_wilt/analysis.R
+++ Bean_wilt/analysis.R
@@ -3,8 +3,8 @@ library(DESeq2)
 library("BiocParallel")
 register(MulticoreParam(12))
 library(data.table)
-library(dplyr)
 library(plyr)
+library(dplyr)
 library(Biostrings)
 library(devtools)
 load_all("../metabarcoding_pipeline/scripts/myfunctions")
@@ -28,30 +28,38 @@ taxData<-taxData[,c(1,3,5,7,9,11,13,2,4,6,8,10,12,14)]
 # add best "rank" at 0.65 confidence and tidy-up the table
 taxData<-phyloTaxaTidy(taxData,0.65)
 
-# save data into a list, then ubiom_16S$countData to access countData and etc.
-ubiom_16S <- list(
+# save data into a list, then ubiom_BAC$countData to access countData and etc.
+ubiom_BAC <- list(
 	countData=countData,
 	colData=colData[colData,],
-	taxData=taxData
+	taxData=taxData,
+	RHB="BAC"
 )
 
-# remove 18S level from colData
-ubiom_16S$colData$Loci <- droplevels(ubiom_16S$colData$Loci)
-
-# or all in one
-ubiom_ITS <- list(
+# Fungi all in one call
+ubiom_FUN <- list(
 	countData=read.table("FUN.zotus_table.txt",header=T,sep="\t",row.names=1,comment.char = ""),
-	colData=colData[colData,],
-	taxData=phyloTaxaTidy(read.table("zFUN.taxa",header=T,sep=",",row.names=1)[,c(1,3,5,7,9,11,13,2,4,6,8,10,12,14)],0.65)
+	colData=read.table("colData",header=T,sep="\t",row.names=1),
+	taxData=phyloTaxaTidy(read.table("zFUN.taxa",header=T,sep=",",row.names=1)[,c(1,3,5,7,9,11,13,2,4,6,8,10,12,14)],0.65),
+	RHB="FUN"
+) 
+
+# Oomycetes all in one call
+ubiom_OO <- list(
+	countData=read.table("OO.zotus_table.txt",header=T,sep="\t",row.names=1,comment.char = ""),
+	colData=read.table("colDataOO",header=T,sep="\t",row.names=1),
+	taxData=phyloTaxaTidy(read.table("zOO.taxa",header=T,sep=",",row.names=1)[,c(1,3,5,7,9,11,13,2,4,6,8,10,12,14)],0.65),
+	RHB="OO"
 ) 
 
 #===============================================================================
 #       Create DEseq objects 
 #===============================================================================
 
-# attach either ITS or 16S objects
-invisible(mapply(assign, names(ubiom_ITS), ubiom_ITS, MoreArgs=list(envir = globalenv())))
-
+# attach objects (FUN, BAC or OO)
+invisible(mapply(assign, names(ubiom_FUN), ubiom_FUN, MoreArgs=list(envir = globalenv())))
+#invisible(mapply(assign, names(ubiom_BAC), ubiom_BAC, MoreArgs=list(envir = globalenv())))
+#invisible(mapply(assign, names(ubiom_OO), ubiom_OO, MoreArgs=list(envir = globalenv())))
 
 # ensure colData rows and countData columns have the same order
 colData <- colData[names(countData),]
@@ -62,10 +70,13 @@ design<-~1
 #create DES object
 dds<-DESeqDataSetFromMatrix(countData,colData,design)
 
-# calculate size factors - using geoMeans function due to DES error
-# every gene contains at least one zero, cannot compute log geometric means
-#sizeFactors(dds) <-sizeFactors(estimateSizeFactors(dds))
-sizeFactors(dds) <-geoMeans(dds)
+# calculate size factors - use geoMeans function if
+# every gene contains at least one zero, as cannot compute log geometric means
+sizeFactors(dds) <-sizeFactors(estimateSizeFactors(dds))
+#sizeFactors(dds) <-geoMeans(dds)
+
+# The seqeuncing run contain additional data, so subset bean data
+dds <- dds[,dds$type=="bean"]
 
 #===============================================================================
 #       PCA plot
@@ -78,22 +89,18 @@ mypca <- des_to_pca(dds)
 df <-t(data.frame(t(mypca$x)*mypca$percentVar))
 
 # plot the PCA
-pdf("all.its.pdf")
-plotOrd(df,dds@colData,design="condition",shape="type")
+pdf("OO.pdf")
+plotOrd(df,dds@colData,design="condition",)
 dev.off()
 
-# bean sample X45_S68 is a clear outlier
+# bean sample X45_S68 is a clear outlier 
 
 #===============================================================================
 #       Differential analysis of bean data
 #===============================================================================
 
-# subset bean data
-dds <- dds[,dds$type=="bean"]
-
 # remove ITS outlier as identified by PCA (no 16S outliers)
 dds <- dds[,colnames(dds)!="X45_S68"]
-
 
 # filter for low counts - this can affect the FD probability and DESeq2 does apply its own filtering for genes/otus with no power 
 # dds<-dds[rowSums(counts(dds,normalize=T))>0,]
@@ -114,13 +121,14 @@ design(dds) <- ~farm + bean + condition + condition:bean
 # calculate fit
 dds <- DESeq(dds,parallel=T)
 
-# contrasts to test
 # main effect
 contrast=c("condition","SICK", "HEALTHY" )
+
+# bean effect
 contrast=c("bean","FRENCH","RUNNER")
 
 # interaction term (both should give roughly the same results - with FC in opposite direction)
-#contrast=list( "beanFRENCH.conditionHEALTHY","beanFrench.conditionHEALTHY") # effect of condition on french beans 
+# contrast=list( "beanFRENCH.conditionHEALTHY","beanFrench.conditionHEALTHY") # effect of condition on french beans 
 contrast=list( "beanRUNNER.conditionHEALTHY","beanRUNNER.conditionSICK") # effect of condition on runner beans
 
 # calculate results
@@ -132,9 +140,9 @@ res.merge <- data.table(inner_join(data.table(OTU=rownames(res),as.data.frame(re
 # show those with BH p <= 0.05
 res.merge[padj<=0.05,]
 
-write.table(res.merge,"main_effect.txt",quote=F,sep="\t",na="",row.names=F)
-write.table(res.merge,"interaction.txt",quote=F,sep="\t",na="")
-write.table(res.merge,"bean_effect.txt",quote=F,sep="\t",na="")
+write.table(res.merge,paste(RHB,"main_effect.txt",sep="_"),quote=F,sep="\t",na="",row.names=F)
+write.table(res.merge,paste(RHB,"interaction.txt",sep="_"),quote=F,sep="\t",na="")
+write.table(res.merge,paste(RHB,"bean_effect.txt",sep="_"),quote=F,sep="\t",na="")
 
 #===============================================================================
 #       Differential OTUs
@@ -145,17 +153,6 @@ myOTUs <- readDNAStringSet("ITS.otus.fa")
 
 writeXStringSet(myOTUs[as.matrix(res.merge[padj<=0.1,1]),"main_big.fa")
 
-#===============================================================================
-#       PCA plots of beans only
-#===============================================================================
-
-mypca <- des_to_pca(dds)
-df <-t(data.frame(t(mypca$x)*mypca$percentVar))
-
-plotOrd(df,dds@colData,design="condition",title="Condition")
-plotOrd(df,dds@colData,design="farm",title="Farm")
-plotOrd(df,dds@colData,design="variety", shape="bean",title="Bean Type")
-plotOrd(df,dds@colData,design="field", shape="variety",title="Field")
 
 
 
