f2394de67b75af1d3a76e6bb00625bbc257c2ce4
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-10-11 11:05:51 +0100

    Update UPARSE.sh

--- ARD/Castle_Farm/Soil/UPARSE.sh
+++ ARD/Castle_Farm/Soil/UPARSE.sh
@@ -6,6 +6,7 @@ $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTU $PROJECT_FOLDE
 
 # FUN
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER $RUN FUN 0 0
+sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/FUN.zotus.fa # workaround for uparse bug
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN FUN
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTU $PROJECT_FOLDER $RUN FUN 23 21
 
