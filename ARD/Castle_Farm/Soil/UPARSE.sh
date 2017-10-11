e529e06333acac7f49d43f9477de850fab8db9a2
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-10-11 11:31:17 +0100

    Update UPARSE.sh

--- ARD/Castle_Farm/Soil/UPARSE.sh
+++ ARD/Castle_Farm/Soil/UPARSE.sh
@@ -19,5 +19,5 @@ $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTU $PROJECT_FOLDE
 # NEM
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER 161025 NEM 0 0
 sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/161025/NEM.zotus.fa # workaround for uparse bug
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER 161025 NEM 
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER 161025 NEM sintax
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTU $PROJECT_FOLDER 161025 NEM 23 18
