d4e844dcadb53018b86956c025d856b57aa11e75
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-10-17 12:49:43 +0100

    Update UPARSE.sh

--- ARD/Frank_Matthews/Rhizosphere/UPARSE.sh
+++ ARD/Frank_Matthews/Rhizosphere/UPARSE.sh
@@ -23,7 +23,7 @@ $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJEC
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTU $PROJECT_FOLDER ../$RUN OO 21 20
 
 # NEM
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER ../$RUN OO 0 0
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER ../$RUN NEM 0 0
 sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/$RUN/NEM.zotus.fa # workaround for uparse bug
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER ../$RUN NEM 
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTU $PROJECT_FOLDER ../$RUN NEM 23 18
