6824650044d9f43442fd16bc66ddaab6f459597a
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-10-17 12:14:55 +0100

    Update UPARSE.sh

--- ARD/Frank_Matthews/Rhizosphere/UPARSE.sh
+++ ARD/Frank_Matthews/Rhizosphere/UPARSE.sh
@@ -6,24 +6,24 @@ mkdir ~/projects/ARD/analysis/data # I've hardcoded some stuff in the pipeline t
 
 # FUN
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER ../$RUN FUN 0 0
-sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/FUN.zotus.fa # workaround for uparse bug
+sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/$RUN/FUN.zotus.fa # workaround for uparse bug
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER ../$RUN FUN
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTU $PROJECT_FOLDER ../$RUN FUN 23 21
 
 # BAC
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER ../$RUN BAC 17 21
-sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/BAC.zotus.fa # workaround for uparse bug
+sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/$RUN/BAC.zotus.fa # workaround for uparse bug
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER ../$RUN BAC
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTU $PROJECT_FOLDER ../$RUN BAC 17 21
 
 # OO
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER ../$RUN OO 0 0
-sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/OO.zotus.fa # workaround for uparse bug
+sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/$RUN/OO.zotus.fa # workaround for uparse bug
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER ../$RUN OO sintax
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTU $PROJECT_FOLDER ../$RUN OO 21 20
 
 # NEM
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER ../$RUN OO 0 0
-sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/NEM.zotus.fa # workaround for uparse bug
+sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/$RUN/NEM.zotus.fa # workaround for uparse bug
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER ../$RUN NEM 
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTU $PROJECT_FOLDER ../$RUN NEM 23 18
