c1234d282a4907406d39766a4c19199f6742941d
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-11-10 16:28:43 +0000

    Update preprocessing.sh

--- ARD/Rootstocks/Rhizosphere/preprocessing.sh
+++ ARD/Rootstocks/Rhizosphere/preprocessing.sh
@@ -1,2 +1,6 @@
-
+# setup links and variables
+PROJECT_FOLDER=~/projects/ARD/analysis
+ln -s ~/pipelines/metabarcoding/ $PROJECT_FOLDER/metabarcoding_pipeline
+RUN="Rootstocks/Rhizosphere"
+mkdir ~/projects/ARD/analysis/data # I've hardcoded some stuff in the pipeline to use a folder called data (need to edit this...) 
 
