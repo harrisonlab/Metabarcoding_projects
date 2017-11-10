86c7b5baacfa086968a05b53ca3dba4b90e6a437
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-11-10 17:00:54 +0000

    Update preprocessing.sh

--- ARD/Rootstocks/Soil/preprocessing.sh
+++ ARD/Rootstocks/Soil/preprocessing.sh
@@ -1,2 +1,8 @@
 # FM samples already preprocessed
 # preprocessing for Shrama samples only
+
+# setup links and variables
+PROJECT_FOLDER=~/projects/ARD/analysis
+ln -s ~/pipelines/metabarcoding/ $PROJECT_FOLDER/metabarcoding_pipeline
+RUN="../Rootstocks/Soil"
+mkdir ~/projects/ARD/analysis/data # I've hardcoded some stuff in the pipeline to use a folder called data (need to edit this...) 
