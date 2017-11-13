627011cf347884bb393a6c6a7f7ab1d203ea7bae
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-11-13 09:31:48 +0000

    Update UPARSE.sh

--- ARD/Rootstocks/Soil/UPARSE.sh
+++ ARD/Rootstocks/Soil/UPARSE.sh
@@ -2,18 +2,22 @@
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER $RUN FUN 0 0
 sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/FUN.zotus.fa # workaround for uparse bug
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN FUN
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN FUN
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTU $PROJECT_FOLDER $RUN FUN 23 21
 
 # BAC
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER $RUN BAC 0 0
 sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/BAC.zotus.fa # workaround for uparse bug
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN BAC
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN BAC
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTU $PROJECT_FOLDER $RUN BAC 0 0
 
 # OO
 cp $PROJECT_FOLDER/FM/Stoolbeds/OO*  $PROJECT_FOLDER/data/$RUN/.
 cp $PROJECT_FOLDER/FM/Stoolbeds/zOO* $PROJECT_FOLDER/data/$RUN/.
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN OO
 
 # NEM
 cp $PROJECT_FOLDER/FM/Stoolbeds/NEM* $PROJECT_FOLDER/data/$RUN/.
 cp $PROJECT_FOLDER/FM/Stoolbeds/zNEM* $PROJECT_FOLDER/data/$RUN/.
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN NEM
