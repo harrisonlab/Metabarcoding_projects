dc2242c07895d816b2b0b590ab1d8ceb987e995c
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-11-21 11:15:23 +0000

    Update analysis.R

--- ARD/Dutch/Herbie/analysis.R
+++ ARD/Dutch/Herbie/analysis.R
@@ -2,7 +2,6 @@
 #       Load libraries
 #===============================================================================
 
-# library(phyloseq)
 library(DESeq2)
 library("BiocParallel")
 register(MulticoreParam(12))
@@ -11,8 +10,6 @@ library(plyr)
 library(dplyr)
 library(devtools)
 load_all("~/pipelines/metabarcoding/scripts/myfunctions")
-# library(cooccur)
-# library(parallel)
 
 #===============================================================================
 #       Load data 
@@ -49,46 +46,11 @@ ubiom_FUN <- list(
 	RHB="FUN"
 ) 
 
-# I've rerun the fungal pipeline, which now uses a different naming convention
-colnames(ubiom_FUN$countData) <- gsub("(^.*_)(S[0-9]*)($)","\\2D170217",colnames(ubiom_FUN$countData)) # \\2 keeps the second match () group
-
-# Oomycetes
-ubiom_OO <- list(
-	countData=read.table("OO.zotus_table.txt",header=T,sep="\t",row.names=1,comment.char = ""),
-	colData=read.table("colData2",header=T,sep="\t",row.names=1),
-	taxData=phyloTaxaTidy(read.table("zOO.taxa",header=F,sep=",",row.names=1)[,c(1,3,5,7,9,11,13,2,4,6,8,10,12,14)],0.65),
-	RHB="OO"
-) 
-
-# Nematodes 
-ubiom_NEM <- list(
-	countData=read.table("NEM.zotus_table.txt",header=T,sep="\t",row.names=1,comment.char = ""),
-	colData=read.table("colData2",header=T,sep="\t",row.names=1),
-	taxData=phyloTaxaTidy(read.table("zNEM.taxa",header=F,sep=",",row.names=1)[,c(1,3,5,7,9,11,13,2,4,6,8,10,12,14)],0.65),
-	RHB="NEM"
-) 
-
 #===============================================================================
 #       Combine species 
 #===============================================================================
 
 #### combine species at 0.95 (default) confidence (if they are species). Works well for Oomycetes and Fungi
-
-# attach OO objects
-invisible(mapply(assign, names(ubiom_OO), ubiom_OO, MoreArgs=list(envir = globalenv())))
-# list of species with more than one associated OTU
-combinedTaxa <- combineTaxa("zOO.taxa")
-# show the list
-combinedTaxa[,1]
-# manual filter list to remove none species (e.g. unknown, Pythium aff)
-combinedTaxa <- combinedTaxa[c(-3,-9),]
-# adjust countData for combined taxa
-countData <- combCounts(combinedTaxa,countData)
-# adjust taxData for combined taxa
-taxData <- combTaxa(combinedTaxa,taxData)
-# copy back to object
-ubiom_OO$countData <- countData
-ubiom_OO$taxData <- taxData
 
 # list of species with more than one associated OTU
 invisible(mapply(assign, names(ubiom_FUN), ubiom_FUN, MoreArgs=list(envir = globalenv())))
@@ -99,24 +61,14 @@ taxData <- combTaxa(combinedTaxa,taxData)
 ubiom_FUN$countData <- countData
 ubiom_FUN$taxData <- taxData
 
-# list of species with more than one associated OTU
-invisible(mapply(assign, names(ubiom_NEM), ubiom_NEM, MoreArgs=list(envir = globalenv())))
-combinedTaxa <- combineTaxa("zNEM.taxa")
-combinedTaxa <- combinedTaxa[c(-3,-6),]
-countData <- combCounts(combinedTaxa,countData)
-taxData <- combTaxa(combinedTaxa,taxData)
-ubiom_NEM$countData <- countData
-ubiom_NEM$taxData <- taxData
 
 #===============================================================================
 #       Attach objects
 #===============================================================================
 
-# attach objects (FUN, BAC,OO or NEM)
+# attach objects (FUN, BAC)
 invisible(mapply(assign, names(ubiom_FUN), ubiom_FUN, MoreArgs=list(envir = globalenv())))
 invisible(mapply(assign, names(ubiom_BAC), ubiom_BAC, MoreArgs=list(envir = globalenv())))
-invisible(mapply(assign, names(ubiom_OO), ubiom_OO, MoreArgs=list(envir = globalenv())))
-invisible(mapply(assign, names(ubiom_NEM), ubiom_NEM, MoreArgs=list(envir = globalenv())))
 
 #===============================================================================
 #       Create DEseq objects 
@@ -124,12 +76,9 @@ invisible(mapply(assign, names(ubiom_NEM), ubiom_NEM, MoreArgs=list(envir = glob
 
 # ensure colData rows and countData columns have the same order
 colData <- colData[names(countData),]
-# Depending on how I've produced the files...
-# row.names(colData) <- colData$name
-# colData <- colData[gsub("\\.","-",sub("_.*","",sub("^X","",names(countData)))),]
 
 # remove low count samples and control samples (not needed here)
-filter <- (colSums(countData)>=1000) & (colData$pair!="C")
+filter <- colSums(countData)>=1000)
 colData <- droplevels(colData[filter,])
 countData <- countData[,filter]
 
@@ -142,21 +91,14 @@ dds<-DESeqDataSetFromMatrix(countData,colData,design)
 
 # calculate size factors - use geoMeans function if
 # every gene contains at least one zero, as cannot compute log geometric means
-# sizeFactors(dds) <-sizeFactors(estimateSizeFactors(dds))
-sizeFactors(dds) <-geoMeans(dds)
+ sizeFactors(dds) <-sizeFactors(estimateSizeFactors(dds))
+# sizeFactors(dds) <-geoMeans(dds)
 # library(edgeR) # I think anyway
 # calcNormFactors(counts(dds),method="RLE",lib.size=(prop.table(colSums(counts(dds)))))
-# Correction from aboslute quantification (BAC only at the moment)
-sizeFactors(dds)<-geoMeans(dds)* sapply(colData$copies,function(x) x/mean(colData$copies,na.rm=T))
-
 
 #===============================================================================
 #       Filter data 
 #============================================================================
-
-### Pythium specific filter to remove OTUs which are unlikely part of the SAR kingdom
-myfilter <- row.names(countData[row.names(countData) %in% row.names(taxData[(taxData$kingdom=="SAR"|as.numeric(taxData$k_conf)<=0.5),]),])
-dds <- dds[myfilter,]
 
 ### read accumulation filter
 # output pdf file
@@ -171,10 +113,9 @@ dev.off()
 #### Select filter ####
 # Apply seperately for appropriate data set depending on cut-off chosen from graph
 myfilter <- dtt$OTU[1:500] #FUN
-myfilter <- dtt$OTU[1:40] # OO
-myfilter <- dtt$OTU[1:75] # NEM
 myfilter <- dtt$OTU[1:4500]  # BAC
 
+myfilter <- dtt$OTU[dtt$CD>5]
 # filter out low abundance OTUs
 dds <- dds[myfilter,]
 
@@ -188,21 +129,14 @@ mypca <- des_to_pca(dds)
 # to get pca plot axis into the same scale create a dataframe of PC scores multiplied by their variance
 df <-t(data.frame(t(mypca$x)*mypca$percentVar))
 
-# Add spatial information as a numeric and plot 
-colData$location<-as.number(colData$pair)
-
 # plot the PCA
-pdf(paste(RHB,"VA.pdf",sep="_"))
-plotOrd(df,colData,design="condition",xlabel="PC1",ylabel="PC2")
-plotOrd(df,colData,shape="condition",design="location",continuous=T,xlabel="PC1",ylabel="PC2")
-dev.off()
+pdf(paste(RHB,"PCA.pdf",sep="_"))
+plotOrd(df,colData,design="condition",shape="block",xlabel="PC1",ylabel="PC2")
 
-### remove spatial information (this uses the factor "pair" not the numeric "location") and plot
-pc.res <- resid(aov(mypca$x~colData$pair,colData))
+### remove/minimise block data
+pc.res <- resid(aov(mypca$x~colData$block,colData))
 df <- t(data.frame(t(pc.res*mypca$percentVar)))
-
-pdf(paste(RHB,"VA_deloc.pdf",sep="_"))
-plotOrd(df,colData,shape="condition",design="location",continuous=T,xlabel="PC1",ylabel="PC2")
+plotOrd(df,colData,design="condition",shape="block",xlabel="PC1",ylabel="PC2")
 dev.off()
 
 #===============================================================================
@@ -217,7 +151,7 @@ dds<-dds[rowSums(counts(dds,normalize=T))>0,]
 alpha <- 0.1
 
 # the full model 
-full_design <- ~pair + condition
+full_design <- ~block + condition
 
 # add full model to dds object
 design(dds) <- full_design
@@ -225,17 +159,10 @@ design(dds) <- full_design
 # calculate fit
 dds <- DESeq(dds,parallel=T)
 
-# contrast (not actually necessary in this case as this would be the default result calculated by results(dds)
-# contrast <- c("condition","S","H")
-res <- results(dds,alpha=alpha,parallel=T)
+contrast <- c("condition","Treated","Untreated") # the default  alculates the contrast the othe rway
+res <- results(dds,alpha=alpha,parallel=T,contrast=contrast)
 res.merge <- data.table(inner_join(data.table(OTU=rownames(res),as.data.frame(res)),data.table(OTU=rownames(taxData),taxData)))
 write.table(res.merge, paste(RHB,"diff_filtered.txt",sep="_"),quote=F,sep="\t",na="",row.names=F)
-
-# MA plot
-pdf(paste(RHB,"ma_plot.pdf",sep="_"))
-plot_ma(res.merge)
-dev.off()
-
 
 #################################################################################
 #################################################################################
