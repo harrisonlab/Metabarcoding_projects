505002c3ef7cccd8c564ae7a4a48cdb0d222a4e9
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-11-27 11:39:31 +0000

    Update UPARSE.sh

--- ARD/Dutch/Koekoek/UPARSE.sh
+++ ARD/Dutch/Koekoek/UPARSE.sh
@@ -10,6 +10,14 @@ sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/$RUN/FUN.zotus.fa # workaround for upars
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER ../$RUN FUN
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTU $PROJECT_FOLDER ../$RUN FUN 23 21
 
+for F in  $PROJECT_FOLDER/$RUN/FUN*otus.fa; do
+  $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c ITS_regions \
+  $PROJECT_FOLDER/$RUN/$F \
+  $PROJECT_FOLDER/metabarcoding_pipeline/hmm/ssu_end.hmm \
+  $PROJECT_FOLDER/metabarcoding_pipeline/hmm/58s_start.hmm \
+  20
+done
+
 # BAC
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER ../$RUN BAC 0 0
 sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/$RUN/BAC.zotus.fa # workaround for uparse bug
