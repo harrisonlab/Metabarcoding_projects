75ee07100aa60e11e3a44e0e38d07b93ff136477
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-12-01 13:43:00 +0000

    Update analysis.R

--- ARD/Dutch/Koekoek/analysis.R
+++ ARD/Dutch/Koekoek/analysis.R
@@ -13,7 +13,7 @@ library(lmPerm)
 library(ggplot2)
 library(devtools)
 load_all("~/pipelines/metabarcoding/scripts/myfunctions")
-
+library(Biostrings)
 #===============================================================================
 #       Load data 
 #===============================================================================
@@ -167,6 +167,10 @@ res <- results(dds2,alpha=alpha,parallel=T)
 res.merge <- data.table(inner_join(data.table(OTU=rownames(res),as.data.frame(res)),data.table(OTU=rownames(taxData),taxData)))
 # write resuts
 write.table(res.merge, paste(RHB,"ARD_candidates.txt",sep="_"),quote=F,sep="\t",na="",row.names=F)
+# output significant otu fasta
+writeXStringSet(readDNAStringSet(paste0(RHB,".zotus.fa"))[res.merge[padj<=0.1]$OTU],paste(RHB,".ARD.sig.fa"))
+
+
 #######
 
 ### test for ARD in all samples ###
@@ -182,6 +186,7 @@ dds2 <- DESeq(dds2,parallel=T)
 res <- results(dds2,alpha=alpha,parallel=T)
 res.merge <- data.table(inner_join(data.table(OTU=rownames(res),as.data.frame(res)),data.table(OTU=rownames(taxData),taxData)))
 write.table(res.merge, paste(RHB,"ARD_candidates_all_samples.txt",sep="_"),quote=F,sep="\t",na="",row.names=F)
+writeXStringSet(readDNAStringSet(paste0(RHB,".zotus.fa"))[res.merge[padj<=0.1]$OTU],paste(RHB,".ARD_all.sig.fa"))
 ######
 
 ### test for difference between tree stations and aisles (fully paired) ###
