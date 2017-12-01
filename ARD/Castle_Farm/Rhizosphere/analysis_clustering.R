bae82cbc2da8577b1ce84bb321b5b92f6a71bf63
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-12-01 15:31:29 +0000

    Create analysis_clustering.R

---
+++ ARD/Castle_Farm/Rhizosphere/analysis_clustering.R
@@ -0,0 +1,230 @@
+#===============================================================================
+#       Load libraries
+#===============================================================================
+
+# library(phyloseq)
+library(DESeq2)
+library("BiocParallel")
+register(MulticoreParam(12))
+library(data.table)
+library(plyr)
+library(dplyr)
+library(ggplot2)
+library(devtools)
+load_all("~/pipelines/metabarcoding/scripts/myfunctions")
+library(Biostrings)
+# library(cooccur)
+# library(parallel)
+
+#===============================================================================
+#       Load data 
+#===============================================================================
+
+# load otu count table
+countData <- read.table("BAC.otus_table.txt",header=T,sep="\t",row.names=1, comment.char = "")
+
+# load sample metadata
+colData <- read.table("colData",header=T,sep="\t",row.names=1)
+
+# load taxonomy data
+taxData <- read.table("BAC.taxa",header=F,sep=",",row.names=1)
+
+# reorder columns
+taxData<-taxData[,c(1,3,5,7,9,11,13,2,4,6,8,10,12,14)]
+
+# add best "rank" at 0.65 confidence and tidy-up the table
+taxData<-phyloTaxaTidy(taxData,0.65)
+
+# save data into a list
+ubiom_BAC <- list(
+	countData=countData,
+	colData=colData,
+	taxData=taxData,
+	RHB="BAC"
+)
+
+# Fungi all in one call
+ubiom_FUN <- list(
+	countData=read.table("FUN.otus_table.txt",header=T,sep="\t",row.names=1,comment.char = ""),
+	colData=read.table("colData",header=T,sep="\t",row.names=1),
+	taxData=phyloTaxaTidy(read.table("FUN.taxa",header=F,sep=",",row.names=1)[,c(1,3,5,7,9,11,13,2,4,6,8,10,12,14)],0.65),
+	RHB="FUN"
+) 
+
+# Oomycetes
+ubiom_OO <- list(
+	countData=read.table("OO.otus_table.txt",header=T,sep="\t",row.names=1,comment.char = ""),
+	colData=read.table("colData2",header=T,sep="\t",row.names=1),
+	taxData=phyloTaxaTidy(read.table("OO.taxa",header=F,sep=",",row.names=1)[,c(1,3,5,7,9,11,13,2,4,6,8,10,12,14)],0.65),
+	RHB="OO"
+) 
+rownames(ubiom_OO$colData) <- paste0("X",gsub("_","\\.",ubiom_OO$colData$name),"_",sub("D.*","",rownames(ubiom_OO$colData)))
+
+# Nematodes 
+ubiom_NEM <- list(
+	countData=read.table("NEM.otus_table.txt",header=T,sep="\t",row.names=1,comment.char = ""),
+	colData=read.table("colData2",header=T,sep="\t",row.names=1),
+	taxData=phyloTaxaTidy(read.table("NEM.taxa",header=F,sep=",",row.names=1)[,c(1,3,5,7,9,11,13,2,4,6,8,10,12,14)],0.65),
+	RHB="NEM"
+) 
+rownames(ubiom_NEM$colData) <- paste0("X",gsub("_","\\.",ubiom_NEM$colData$name),"_",sub("D.*","",rownames(ubiom_NEM$colData)))
+
+#===============================================================================
+#       Combine species 
+#===============================================================================
+
+#### combine species at 0.95 (default) confidence (if they are species). Works well for Oomycetes and Fungi
+
+# attach OO objects
+invisible(mapply(assign, names(ubiom_OO), ubiom_OO, MoreArgs=list(envir = globalenv())))
+# list of species with more than one associated OTU
+combinedTaxa <- combineTaxa("OO.taxa")
+# show the list
+combinedTaxa
+# manual filter list to remove none species (e.g. unknown, Pythium aff)
+combinedTaxa <- combinedTaxa[c(-4,-7,-10),]
+# adjust countData for combined taxa
+countData <- combCounts(combinedTaxa,countData)
+# adjust taxData for combined taxa
+taxData <- combTaxa(combinedTaxa,taxData)
+# copy back to object
+ubiom_OO$countData <- countData
+ubiom_OO$taxData <- taxData
+
+# Fungi
+invisible(mapply(assign, names(ubiom_FUN), ubiom_FUN, MoreArgs=list(envir = globalenv())))
+combinedTaxa <- combineTaxa("FUN.taxa")
+countData <- combCounts(combinedTaxa,countData)
+taxData <- combTaxa(combinedTaxa,taxData)
+ubiom_FUN$countData <- countData
+ubiom_FUN$taxData <- taxData
+
+# Nematodes
+invisible(mapply(assign, names(ubiom_NEM), ubiom_NEM, MoreArgs=list(envir = globalenv())))
+combinedTaxa <- combineTaxa("NEM.taxa")
+combinedTaxa <- combinedTaxa[-2,]
+countData <- combCounts(combinedTaxa,countData)
+taxData <- combTaxa(combinedTaxa,taxData)
+ubiom_NEM$countData <- countData
+ubiom_NEM$taxData <- taxData
+
+#===============================================================================
+#       Attach objects
+#===============================================================================
+
+# attach objects (FUN, BAC,OO or NEM)
+invisible(mapply(assign, names(ubiom_FUN), ubiom_FUN, MoreArgs=list(envir = globalenv())))
+invisible(mapply(assign, names(ubiom_BAC), ubiom_BAC, MoreArgs=list(envir = globalenv())))
+invisible(mapply(assign, names(ubiom_OO), ubiom_OO, MoreArgs=list(envir = globalenv())))
+invisible(mapply(assign, names(ubiom_NEM), ubiom_NEM, MoreArgs=list(envir = globalenv())))
+
+#===============================================================================
+#       Create DEseq objects 
+#===============================================================================
+
+# ensure colData rows and countData columns have the same order
+colData <- colData[names(countData),]
+
+# remove low count samples and control samples (not needed here)
+filter <- (colSums(countData)>=1000)&colData$condition!="C"
+colData <- droplevels(colData[filter,])
+countData <- countData[,filter]
+
+# simple Deseq design
+design<-~1
+
+#create DES object
+# colnames(countData) <- row.names(colData)
+dds<-DESeqDataSetFromMatrix(countData,colData,design)
+
+# calculate size factors - use geoMeans function if
+# every gene contains at least one zero, as cannot compute log geometric means
+sizeFactors(dds) <-sizeFactors(estimateSizeFactors(dds))
+# sizeFactors(dds) <-geoMeans(dds)
+# library(edgeR) # I think anyway
+# calcNormFactors(counts(dds),method="RLE",lib.size=(prop.table(colSums(counts(dds)))))
+
+# Correction from aboslute quantification
+sizeFactors(dds) <- sizeFactors(dds)/colData$funq
+sizeFactors(dds) <- sizeFactors(dds)/colData$bacq
+
+#===============================================================================
+#       Filter data 
+#============================================================================
+
+### filters to remove OTUs which are unlikely part of the correct kingdom (SAR and 18S Eukaryote)
+# pythium
+myfilter <- row.names(countData[row.names(countData) %in% row.names(taxData[(taxData$kingdom=="SAR"|as.numeric(taxData$k_conf)<=0.5),]),])
+dds <- dds[myfilter,]
+# nematode
+myfilter <- row.names(taxData[as.number(taxData$c_conf)>0.9 & as.number(taxData$o_conf)>0.9,])
+dds <- dds[rownames(dds)%in%myfilter,]
+
+### read accumulation filter
+# plot cummulative reads (will also produce a data table "dtt" in the global environment)
+ggsave(paste(RHB,"OTU_counts.pdf",sep="_"),plotCummulativeReads(counts(dds,normalize=T)))
+
+#### Select filter ####
+# Apply seperately for appropriate data set depending on cut-off chosen from graph
+myfilter <- dtt$OTU[1:500] #FUN
+myfilter <- dtt$OTU[1:75] # OO
+myfilter <- dtt$OTU[1:75] # NEM
+myfilter <- dtt$OTU[1:4500]  # BAC
+
+# filter out low abundance OTUs
+dds <- dds[myfilter,]
+
+#===============================================================================
+#       PCA plot
+#===============================================================================
+
+# perform PC decomposition of DES object
+mypca <- des_to_pca(dds)
+
+# to get pca plot axis into the same scale create a dataframe of PC scores multiplied by their variance
+df <-t(data.frame(t(mypca$x)*mypca$percentVar))
+
+# Add spatial information as a numeric and plot 
+colData$location<-as.number(colData$pair)
+
+# plot the PCA
+pdf(paste(RHB,"PCA.pdf",sep="_"))
+plotOrd(df,colData,design="condition",xlabel="PC1",ylabel="PC2")
+plotOrd(df,colData,shape="condition",design="location",continuous=T,xlabel="PC1",ylabel="PC2")
+dev.off()
+
+### remove spatial information (this uses the factor "pair" not the numeric "location") and plot
+pc.res <- resid(aov(mypca$x~colData$pair,colData))
+df <- t(data.frame(t(pc.res*mypca$percentVar)))
+
+pdf(paste(RHB,"PCA_deloc.pdf",sep="_"))
+plotOrd(df,colData,shape="condition",design="location",continuous=T,xlabel="PC1",ylabel="PC2")
+dev.off()
+
+#===============================================================================
+#       differential analysis
+#===============================================================================
+ 
+# filter for low counts - this can affect the FD probability and DESeq2 does apply its own filtering for genes/otus with no power 
+# but, no point keeping OTUs with 0 count
+dds<-dds[rowSums(counts(dds,normalize=T))>0,]
+
+# p value for FDR cutoff
+alpha <- 0.1
+
+# the full model 
+full_design <- ~pair + condition
+
+# add full model to dds object
+design(dds) <- full_design
+
+# calculate fit
+dds <- DESeq(dds,parallel=T)
+
+# contrast (not actually necessary in this case as this would be the default result calculated by results(dds)
+# contrast <- c("condition","S","H")
+res <- results(dds,alpha=alpha,parallel=T)
+res.merge <- data.table(inner_join(data.table(OTU=rownames(res),as.data.frame(res)),data.table(OTU=rownames(taxData),taxData)))
+write.table(res.merge, paste(RHB,"diff.txt",sep="_"),quote=F,sep="\t",na="",row.names=F)
+# output sig fasta
+writeXStringSet(readDNAStringSet(paste0(RHB,".otus.fa"))[res.merge[padj<=0.1]$OTU],paste0(RHB,".sig.fa"))
