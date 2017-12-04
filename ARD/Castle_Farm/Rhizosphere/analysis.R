2681a4a5519eb15ceee79bb7853c6cf85df3c0f0
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-12-04 12:19:46 +0000

    Update analysis.R

--- ARD/Castle_Farm/Rhizosphere/analysis.R
+++ ARD/Castle_Farm/Rhizosphere/analysis.R
@@ -162,7 +162,7 @@ dds <- dds[rownames(dds)%in%myfilter,]
 
 ### read accumulation filter
 # plot cummulative reads (will also produce a data table "dtt" in the global environment)
-ggsave(paste(RHB,"OTU_counts.pdf",sep="_"),plotCummulativeReads(counts(dds,normalize=T)))
+ggsave(paste(RHB,"v3_OTU_counts.pdf",sep="_"),plotCummulativeReads(counts(dds,normalize=T)))
 
 #### Select filter ####
 # Apply seperately for appropriate data set depending on cut-off chosen from graph
@@ -188,7 +188,7 @@ df <-t(data.frame(t(mypca$x)*mypca$percentVar))
 colData$location<-as.number(colData$pair)
 
 # plot the PCA
-pdf(paste(RHB,"PCA.pdf",sep="_"))
+pdf(paste(RHB,"v3_PCA.pdf",sep="_"))
 plotOrd(df,colData,design="condition",xlabel="PC1",ylabel="PC2")
 plotOrd(df,colData,shape="condition",design="location",continuous=T,xlabel="PC1",ylabel="PC2")
 dev.off()
@@ -197,7 +197,7 @@ dev.off()
 pc.res <- resid(aov(mypca$x~colData$pair,colData))
 df <- t(data.frame(t(pc.res*mypca$percentVar)))
 
-pdf(paste(RHB,"PCA_deloc.pdf",sep="_"))
+pdf(paste(RHB,"v3_PCA_deloc.pdf",sep="_"))
 plotOrd(df,colData,shape="condition",design="location",continuous=T,xlabel="PC1",ylabel="PC2")
 dev.off()
 
@@ -225,6 +225,6 @@ dds <- DESeq(dds,parallel=T)
 # contrast <- c("condition","S","H")
 res <- results(dds,alpha=alpha,parallel=T)
 res.merge <- data.table(inner_join(data.table(OTU=rownames(res),as.data.frame(res)),data.table(OTU=rownames(taxData),taxData)))
-write.table(res.merge, paste(RHB,"diff.txt",sep="_"),quote=F,sep="\t",na="",row.names=F)
+write.table(res.merge, paste(RHB,"v3_diff.txt",sep="_"),quote=F,sep="\t",na="",row.names=F)
 # output sig fasta
-writeXStringSet(readDNAStringSet(paste0(RHB,".zotus.fa"))[res.merge[padj<=0.1]$OTU],paste0(RHB,".sig.fa"))
+writeXStringSet(readDNAStringSet(paste0(RHB,".zotus.fa"))[res.merge[padj<=0.1]$OTU],paste0(RHB,".v3.sig.fa"))
