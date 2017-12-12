d45424f937fd4e9fbb712169082e83bd6f036ecf
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-12-12 17:29:43 +0000

    Update piechart.R

--- ARD/Castle_Farm/Rhizosphere/piechart.R
+++ ARD/Castle_Farm/Rhizosphere/piechart.R
@@ -9,9 +9,10 @@ X <- X %>% group_by(type) %>% mutate(pos = 100-(cumsum(all)- all/2))
 
 g <- ggplot(X,aes(x="",y=all,fill=Phylum,label = sprintf("%0.1f", round(all, digits = 1))))
 g <- g + geom_bar(width = 1,stat="identity")
+g <- g + scale_fill_viridis(discrete=TRUE,direction=-1)
 g <- g + geom_text(data=X[X$all>4,],aes(y = pos),size=3)
 #g <- g +geom_text(aes(label = sprintf("%0.2f", round(all, digits = 2))), position = position_stack(vjust = 0.5,),size=2)
-g <- g + geom_label_repel(data=X[X$all<=4,],aes(y = pos), size=3, show.legend = F, nudge_x=0.75,nudge_y=0.75,segment.size=0.1)# + guides(fill = guide_legend(title = "rank"))
+g <- g + geom_label_repel(data=X[X$all<=4,],aes(y = pos), size=3, show.legend = F,colour="grey", nudge_x=0.75,nudge_y=0.75,segment.size=0.1)# + guides(fill = guide_legend(title = "rank"))
 g <- g + coord_polar(theta="y")
 g <- g +facet_grid(facets=. ~ type,switch="x")
 g <- g + theme_blank() %+replace% theme(	
@@ -21,5 +22,5 @@ g <- g + theme_blank() %+replace% theme(
   strip.background = element_blank(),
   #strip.text.x = element_blank()
 )
-ggsave("test.pdf",g+xlab(NULL)+ylab(NULL))
+ggsave("FUN_pie.pdf",g+xlab(NULL)+ylab(NULL))
 
