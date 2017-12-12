eb4ec95fdf670229b6c73f0c0b88dc67fd3e2e50
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-12-12 17:03:41 +0000

    Update piechart.R

--- ARD/Castle_Farm/Rhizosphere/piechart.R
+++ ARD/Castle_Farm/Rhizosphere/piechart.R
@@ -3,10 +3,23 @@ Y <- sumTaxa(list(as.data.frame(counts(dds,normalized=T)),taxData,colData(dds)),
 X$type=1
 Y$type=2
 X <- rbind(X,Y)
+X$Phylum <- sub("\\(.*","",X$rank)
 
-g <- ggplot(X,aes(x=factor(1),y=all,fill=rank))
+X <- X %>% group_by(type) %>% mutate(pos = 100-(cumsum(all)- all/2))
+
+g <- ggplot(X,aes(x="",y=all,fill=Phylum,label = sprintf("%0.1f", round(all, digits = 1))))
 g <- g + geom_bar(width = 1,stat="identity")
-g <-g +facet_grid(facets=. ~ type)
+g <- g + geom_text(data=X[X$all>4,],aes(y = pos),size=3)
+#g <- g +geom_text(aes(label = sprintf("%0.2f", round(all, digits = 2))), position = position_stack(vjust = 0.5,),size=2)
+g <- g + geom_label_repel(data=X[X$all<=4,],aes(y = pos), size=3, show.legend = F, nudge_x=0.75,nudge_y=0.75,segment.size=0.1)# + guides(fill = guide_legend(title = "rank"))
 g <- g + coord_polar(theta="y")
-g
-dev.off()
+g <- g +facet_grid(facets=. ~ type,switch="x")
+g <- g + theme_blank() %+replace% theme(	
+#  panel.border = element_rect(colour = "black", fill=NA, size=0.5),
+  axis.text  = element_blank(),
+  axis.ticks = element_blank(),
+  strip.background = element_blank(),
+  #strip.text.x = element_blank()
+)
+ggsave("test.pdf",g+xlab(NULL)+ylab(NULL))
+
