59ffd552d24a3c732f0c9e34c258b5cbf30a4343
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-02-15 16:53:41 +0000

    Update preprocessing.sh

--- ARD/HG/2017/preprocessing.sh
+++ ARD/HG/2017/preprocessing.sh
@@ -4,19 +4,12 @@ PROJECT_FOLDER=~/projects/metabarcoding/ARD
 # sequencer run folders 
 RUNS="180105 180109 180112 180119"
 
-# common folders
+# make project folders
 for RUN in $RUNS; do
-  #mkdir -p $PROJECT_FOLDER/data/$RUN/fastq
+  mkdir -p $PROJECT_FOLDER/data/$RUN/fastq
   mkdir $PROJECT_FOLDER/data/$RUN/quality
   mkdir $PROJECT_FOLDER/data/$RUN/ambiguous
-done
-
-# variable to hold folder names (BAC and FUN)
-RIB="BAC FUN OO NEM"
-
-# loop through the RIB variable, i.e. s = BAC on first loop, S= FUN on second loop, and create the folders
-for RUN in $RUNS; do
-  for s in $RIB; do
+  for s in BAC FUN OO NEM; do
     mkdir -p $PROJECT_FOLDER/data/$RUN/$s/fastq
     mkdir $PROJECT_FOLDER/data/$RUN/$s/filtered
     mkdir $PROJECT_FOLDER/data/$RUN/$s/unfiltered
@@ -24,44 +17,107 @@ for RUN in $RUNS; do
   done
 done
 
-# QC
+# quality check
 for RUN in $RUNS; do
-  for FILE in $PROJECT_FOLDER/data/$RUN/fastq/*; do
+  for FILE in $PROJECT_FOLDER/data/$RUN/fastq/*; do 
     $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c qcheck $FILE $PROJECT_FOLDER/data/$RUN/quality
   done
 done
 
-# BAC and FUN are multiplexed. Can seperate by the primer sequences (p1 for BAC, p2 for FUN)
+# demultiplex (BAC and FUN)
 P1F=CCTACGGGNGGCWGCAG
 P1R=GACTACHVGGGTATCTAATCC
 P2F=CTTGGTCATTTAGAGGAAGTAA
 P2R=ATATGCTTAAGTTCAGCGGG
 
-# demultiplex with 0 difference in primer seqeunce
 for RUN in $RUNS; do
   $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c demultiplex \
-   "$PROJECT_FOLDER/data/$RUN/fastq/*R1*.gz" 0 \
+   "$PROJECT_FOLDER/data/$RUN/fastq/*16S*_R1_*" 0 \
    $P1F $P1R $P2F $P2R
 done
 
-mv $PROJECT_FOLDER/data/$RUN/fastq/*ps1* $PROJECT_FOLDER/data/$RUN/16S/fastq/.
-mv $PROJECT_FOLDER/data/$RUN/fastq/*ps2* $PROJECT_FOLDER/data/$RUN/ITS/fastq/.
-mv $PROJECT_FOLDER/data/$RUN/fastq/*ambig* $PROJECT_FOLDER/data/$RUN/ambiguous/.
-
-# pre-process 16S files (min length 300, max diffs 5, quality 0.5)
 for RUN in $RUNS; do
-  $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c 16Spre \
-   "$PROJECT_FOLDER/data/$RUN/16S/fastq/*R1*.fastq" \
-   $PROJECT_FOLDER/data/$RUN/16S \
-   $PROJECT_FOLDER/metabarcoding_pipeline/primers/adapters.db \
-   300 5 0.5 0 0
+  mv $PROJECT_FOLDER/data/$RUN/fastq/*ps1* $PROJECT_FOLDER/data/$RUN/BAC/fastq/.
+  mv $PROJECT_FOLDER/data/$RUN/fastq/*ps2* $PROJECT_FOLDER/data/$RUN/FUN/fastq/.
+  mv $PROJECT_FOLDER/data/$RUN/fastq/*ambig* $PROJECT_FOLDER/data/$RUN/ambiguous/.
 done
 
-# Pre-process FUN files (min length 200, MAX length 300, quality 1)
+# demultiplex (NEM and OO)
+P1F=CGCGAATRGCTCATTACAACAGC
+P1R=GGCGGTATCTGATCGCC
+P2F=GAAGGTGAAGTCGTAACAAGG
+P2R=AGCGTTCTTCATCGATGTGC
+
+for RUN in $RUNS; do
+  $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c demultiplex \
+   "$PROJECT_FOLDER/data/$RUN/fastq/*Oom*_R1_*" 0 \
+   $P1F $P1R $P2F $P2R
+done
+
+for RUN in $RUNS; do
+  mv $PROJECT_FOLDER/data/$RUN/fastq/*ps1* $PROJECT_FOLDER/data/$RUN/BAC/fastq/.
+  mv $PROJECT_FOLDER/data/$RUN/fastq/*ps2* $PROJECT_FOLDER/data/$RUN/FUN/fastq/.
+  mv $PROJECT_FOLDER/data/$RUN/fastq/*ambig* $PROJECT_FOLDER/data/$RUN/ambiguous/.
+done
+
+
+
+# BACTERIA
+for RUN in $RUNS; do
+  $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c 16Spre \
+   "$PROJECT_FOLDER/data/$RUN/BAC/fastq/*R1*.fastq" \
+   $PROJECT_FOLDER/data/$RUN/BAC \
+   $PROJECT_FOLDER/metabarcoding_pipeline/primers/adapters.db \
+   300 5 0.5 17 21 
+done 
+
+ # FUNGI
 for RUN in $RUNS; do
   $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c ITSpre \
    "$PROJECT_FOLDER/data/$RUN/FUN/fastq/*R1*.fastq" \
-   $PROJECT_FOLDER/data/$RUN/ITS \
+   $PROJECT_FOLDER/data/$RUN/FUN \
    $PROJECT_FOLDER/metabarcoding_pipeline/primers/primers.db \
-   200 1 0 0
+   200 1 23 21
+done
+
+for RUN in $RUNS; do
+  for F in $PROJECT_FOLDER/data/$RUN/FUN/fasta/*_R1.fa; do 
+   FO=$(awk -F"/" '{print $NF}' <<< $F|awk -F"_" '{print $1".r1.fa"}'); 
+   L=$(awk -F"/" '{print $NF}' <<< $F|awk -F"_" '{print $1}') ;
+   echo $L
+   awk -v L=$L '/>/{sub(".*",">"L"."(++i))}1' $F > $FO.tmp && mv $FO.tmp $PROJECT_FOLDER/data/$RUN/FUN/filtered/$FO;
+  done
+done
+
+# Oomycetes
+for RUN in $RUNS; do
+  $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OOpre \
+   "$PROJECT_FOLDER/data/$RUN/OO/fastq/*R1*.fastq" \
+   $PROJECT_FOLDER/data/$RUN/OO \
+   $PROJECT_FOLDER/metabarcoding_pipeline/primers/adapters.db \
+   150 5 0.5 21 20
+done    
+
+# Nematode
+for RUN in $RUNS; do
+  $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c NEMpre \
+   "$PROJECT_FOLDER/data/$RUN/NEM/fastq/*R1*.fastq" \
+   $PROJECT_FOLDER/data/$RUN/NEM \
+   $PROJECT_FOLDER/metabarcoding_pipeline/primers/nematode.db \
+   150 10 0.5 23 18
+done
+
+# make analysis folders
+mkdir $PROJECT_FOLDER/analysis/WP3
+for s in BAC FUN OO NEM; do
+  mkdir -p $PROJECT_FOLDER/analysis/WP3/$s/filtered
+  mkdir $PROJECT_FOLDER/analysis/WP3/$s/unfiltered
+done
+
+# link processed data to anaylsis folders
+for RUN in $RUNS; do
+  for s in BAC FUN OO NEM; do
+    ln -s $PROJECT_FOLDER/data/$RUN/$s/unfiltered/* $PROJECT_FOLDER/analysis/WP3/$s/unfiltered/.
+    ln -s $PROJECT_FOLDER/data/$RUN/$s/filtered/* $PROJECT_FOLDER/analysis/WP3/$s/filtered/.
+  done
 done
