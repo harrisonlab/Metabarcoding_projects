d8342d4cf3677d7da7bb352e6d3062f8e581140a
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-03-16 16:06:50 +0000

    Update analysis.R

--- ARD/WP3/analysis.R
+++ ARD/WP3/analysis.R
@@ -19,4 +19,45 @@ register(MulticoreParam(12))
 load_all("~/pipelines/metabarcoding/scripts/myfunctions")
 environment(plot_ordination) <- environment(ordinate) <- environment(plot_richness) <- environment(phyloseq::ordinate)
 
+#===============================================================================
+#       Load data
+#===============================================================================
 
+ubiom_BAC <- loadData("BAC.otus_table.txt","colData","BAC.taxa","BAC.phy",RHB="BAC")
+ubiom_FUN <- loadData("FUN.otus_table.txt","colData","FUN.taxa","FUN.phy",RHB="FUN")
+
+A1 <- fread("ambiguous1") # fungi r1
+A2 <- fread("ambiguous1") # bacteria r1
+A3 <- fread("ambiguous1") # bacteria merged
+A4 <- fread("ambiguous1") # fungi merged
+
+ubiom_BAC$countData <- as.data.table(rbind.fill(ubiom_BAC$countData,A2,A3))[,lapply(.SD,sum,na.rm=T),by="#OTU ID"] 
+ubiom_FUN$countData <- as.data.table(rbind.fill(ubiom_BAC$countData,A1,A4))[,lapply(.SD,sum,na.rm=T),by="#OTU ID"] 
+
+#===============================================================================
+#       Combine species
+#===============================================================================
+
+#### combine species at 0.95 (default) confidence (if they are species)
+# Fungi
+invisible(mapply(assign, names(ubiom_FUN), ubiom_FUN, MoreArgs=list(envir = globalenv())))
+combinedTaxa <- combineTaxa("FUN.taxa")
+countData <- combCounts(combinedTaxa,countData)
+taxData <- combTaxa(combinedTaxa,taxData)
+ubiom_FUN$countData <- countData
+ubiom_FUN$taxData <- taxData
+
+#===============================================================================
+#       Create DEseq objects
+#===============================================================================
+
+ubiom_FUN$dds <- ubiom_to_des(ubiom_FUN,filter=expression(colSums(countData)>=1000&colData$Block!="R"))
+ubiom_BAC$dds <- ubiom_to_des(ubiom_BAC,filter=expression(colSums(countData)>=1000&colData$Block!="R"))
+
+#===============================================================================
+#       Attach objects
+#===============================================================================
+
+# attach objects (FUN, BAC,OO or NEM)
+invisible(mapply(assign, names(ubiom_FUN), ubiom_FUN, MoreArgs=list(envir = globalenv())))
+invisible(mapply(assign, names(ubiom_BAC), ubiom_BAC, MoreArgs=list(envir = globalenv())))
