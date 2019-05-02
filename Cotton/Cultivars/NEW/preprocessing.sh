67cad45399e6a63f8f7695af78c450d1e0b865bc
Greg Deakin <GregDeakin@users.noreply.github.com>
2019-05-02 10:45:56 +0100

    Update preprocessing.sh

--- Cotton/Cultivars/NEW/preprocessing.sh
+++ Cotton/Cultivars/NEW/preprocessing.sh
@@ -1,1 +1,41 @@
+PROJECT_FOLDER=~/projects/Cotton/
+mkdir -p $PROJECT_FOLDER
+ln -s $MBPL $PROJECT_FOLDER/metabarcoding_pipeline 
 
+RUN=.
+
+for s in BAC FUN; do
+  mkdir -p $PROJECT_FOLDER/data/$RUN/$s/fastq
+  mkdir $PROJECT_FOLDER/data/$RUN/$s/filtered
+  mkdir $PROJECT_FOLDER/data/$RUN/$s/unfiltered
+  mkdir $PROJECT_FOLDER/data/$RUN/$s/fasta
+done
+
+#bacteria
+SSU=BAC
+FPL=17
+RPL=21
+
+MINL=300
+MINOVER=5
+QUAL=0.5
+
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c 16Spre \
+ "$PROJECT_FOLDER/data/$RUN/$SSU/fastq/*R1*.fastq" \
+ $PROJECT_FOLDER/data/$RUN/$SSU \
+ $PROJECT_FOLDER/metabarcoding_pipeline/primers/adapters.db \
+ $MINL $MINOVER $QUAL $FPL $RPL 
+
+#Fungi
+SSU=FUN
+FPL=23 
+RPL=21
+
+MINL=200
+QUAL=1
+
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c ITSpre \
+ "$PROJECT_FOLDER/data/$RUN/$SSU/fastq/*R1*.fastq" \
+ $PROJECT_FOLDER/data/$RUN/$SSU \
+ $PROJECT_FOLDER/metabarcoding_pipeline/primers/primers.db \
+ $MINL $QUAL $FPL $RPL
