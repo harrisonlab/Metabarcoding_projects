1c7705d15d11a7528737b09f473387e20172e639
Greg Deakin <GregDeakin@users.noreply.github.com>
2019-10-22 14:18:13 +0100

    Update UPARSE.sh

--- Endophytes/Leone/UPARSE.sh
+++ Endophytes/Leone/UPARSE.sh
@@ -9,11 +9,11 @@ $PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c UPARSE $PROJ
 sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/FUN.zotus.fa  # workaround for uparse bug
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN FUN
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN FUN 
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/slurm/PIPELINE.sh -c OTUS $PROJECT_FOLDER $RUN FUN 23 21 FUN.otus.fa
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/slurm/PIPELINE.sh -c OTU $PROJECT_FOLDER $RUN FUN 23 21 FUN.otus.fa
 
 # BAC
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c UPARSE $PROJECT_FOLDER $RUN BAC 0 0
 sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/BAC.zotus.fa
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN BAC
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN BAC
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c OTUS $PROJECT_FOLDER $RUN BAC 17 21 BAC.otus.fa
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c OTU $PROJECT_FOLDER $RUN BAC 17 21 BAC.otus.fa
