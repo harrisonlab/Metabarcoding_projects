10c33c2dea58a4fe8b959201942dc51d94e16f1a
Greg Deakin <GregDeakin@users.noreply.github.com>
2019-10-22 14:07:52 +0100

    Update UPARSE.sh

--- Endophytes/Leone/UPARSE.sh
+++ Endophytes/Leone/UPARSE.sh
@@ -4,14 +4,16 @@ PROJECT_FOLDER=~/projects/Endophytes
 # sequencer run folder 
 RUN=leone_230919
 
-## FUN
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER $RUN FUN 0 0
-sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/FUN.zotus.fa # workaround for uparse bug
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN FUN sintax
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTU $PROJECT_FOLDER $RUN FUN 0 0
+# FUN
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c UPARSE $PROJECT_FOLDER $RUN FUN 0 0
+sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/FUN.zotus.fa  # workaround for uparse bug
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN FUN
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN FUN 
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/slurm/PIPELINE.sh -c OTUS $PROJECT_FOLDER $RUN FUN 23 21 FUN.otus.fa
 
-## BAC
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER $RUN BAC 0 0
-sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/BAC.zotus.fa # workaround for uparse bug
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN BAC
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTU $PROJECT_FOLDER $RUN BAC 0 0
+# BAC
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c UPARSE $PROJECT_FOLDER $RUN BAC 0 0
+sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/BAC.zotus.fa
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN BAC
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN BAC
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c OTUS $PROJECT_FOLDER $RUN BAC 17 21 BAC.otus.fa
