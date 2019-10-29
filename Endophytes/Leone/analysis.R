fbca332e5fc7133df815d699412ee86a82f12644
Greg Deakin <GregDeakin@users.noreply.github.com>
2019-10-29 11:59:55 +0000

    Create analysis.R

---
+++ Endophytes/Leone/analysis.R
@@ -0,0 +1,226 @@
+#===============================================================================
+#       Load libraries
+#===============================================================================
+
+library(DESeq2)
+library(BiocParallel)
+library(data.table)
+library(plyr)
+library(tidyverse)
+library(metafuncs)
+library(vegan)
+library(lmPerm)
+
+#environment(plot_ordination) <- environment(ordinate) <- environment(plot_richness) <- environment(phyloseq::ordinate)
+#assignInNamespace("plot_ordination",value=plot_ordination,ns="phyloseq")
+
+#===============================================================================
+#       Load data
+#===============================================================================
+
+# load otu count table
+countData <- read.table("BAC.otus_table.txt",header=T,sep="\t",row.names=1, comment.char = "")
+
+# load sample metadata
+colData <- read.table("colData",header=T,sep="\t",row.names=1)
+
+# load taxonomy data
+taxData <- read.table("BAC.taxa",header=F,sep=",",row.names=1)
+
+# reorder columns
+taxData<-taxData[,c(1,3,5,7,9,11,13,2,4,6,8,10,12,14)]
+
+# add best "rank" at 0.65 confidence and tidy-up the table
+#taxData<-phyloTaxaTidy(taxData,0.65)
+
+# get unifrac dist
+#phylipData <- fread.phylip("BAC.phy")
+
+#njtree <- nj(as.dist(phylipData))
+
+# save data into a list
+ubiom_BAC <- list(
+  countData=countData,
+  colData=colData,
+  taxData=taxData,
+ # phylipData=phylipData,
+  #njtree=njtree,
+  RHB="BAC"
+)
+
+
+# Fungi all in one call
+ubiom_FUN <- list(
+  countData=read.table("FUN.otus_table.txt",header=T,sep="\t",row.names=1,comment.char = ""),
+  colData=read.table("colData",header=T,sep="\t",row.names=1),
+  taxData=phyloTaxaTidy(read.table("FUN.taxa",header=F,sep=",",row.names=1)[,c(1,3,5,7,9,11,13,2,4,6,8,10,12,14)],0.65),
+  RHB="FUN"
+)
+
+
+#===============================================================================
+#       Combine species
+#===============================================================================
+
+#### combine species at 0.95 (default) confidence (if they are species)
+
+# Fungi
+invisible(mapply(assign, names(ubiom_FUN), ubiom_FUN, MoreArgs=list(envir = globalenv())))
+combinedTaxa <- combineTaxa("FUN.taxa",column_order=c(1:7,9:15))
+countData <- combCounts(combinedTaxa,countData)
+taxData <- combTaxa(combinedTaxa,taxData)
+ubiom_FUN$countData <- countData
+ubiom_FUN$taxData <- taxData
+
+#===============================================================================
+#      ****FUNGI/BACTERIA****
+#===============================================================================
+
+# Fungi
+invisible(mapply(assign, names(ubiom_FUN), ubiom_FUN, MoreArgs=list(envir = globalenv())))
+# Bacteria
+invisible(mapply(assign, names(ubiom_BAC), ubiom_BAC, MoreArgs=list(envir = globalenv())))
+
+#===============================================================================
+#       Create DEseq objects
+#===============================================================================
+
+# ensure colData rows and countData columns have the same order
+colData <- colData[names(countData),]
+
+design<-~1
+
+#create DES object
+# colnames(countData) <- row.names(colData)
+dds<-DESeqDataSetFromMatrix(countData,colData,design)
+
+sizeFactors(dds) <-sizeFactors(estimateSizeFactors(dds))
+
+#===============================================================================
+#       Filter data
+#============================================================================
+
+# plot cummulative reads (will also produce a data table "dtt" in the global environment)
+# ggsave(paste(RHB,"OTU_counts.pdf",sep="_"),plotCummulativeReads(counts(dds,normalize=T)))
+
+dds <- dds[rowSums(counts(dds, normalize=T))>4,]
+
+#===============================================================================
+#       Beta diversity PCA/NMDS
+#===============================================================================
+
+### PCA ###
+
+# perform PC decomposition of DES object
+mypca <- des_to_pca(dds)
+
+# to get pca plot axis into the same scale create a dataframe of PC scores multiplied by their variance
+d <-t(data.frame(t(mypca$x)*mypca$percentVar))
+
+# plot the PCA
+plotOrd(d,colData,design="parent",shape="sex",alpha=0.75,cbPalette=T)
+plotOrd(d,colData,design="parent",shape="sex",axes=c(2,3),alpha=0.75,cbPalette=T)
+
+
+#===============================================================================
+#       differential analysis
+#===============================================================================
+
+# p value for FDR cutoff
+alpha <- 0.1
+
+# the model
+design <- ~condition
+#or
+design <- ~block_pair + condition
+
+
+# split dds object into per wood
+# first get rid of bigwood as it only has 2 samples - and remove it from the levels of site
+list_dds <-list(Attingham   = dds[,dds$site=="Attingham"],
+		            Gt_Monk     = dds[,dds$site=="Gt_Monk"],
+		            Langdale    = dds[,dds$site=="Langdale"],
+		            Winding     = dds[,dds$site=="Winding"],
+		            Chestnuts   = dds[,dds$site=="Chestnuts"],
+		            Bigwood   = dds[,dds$site=="Bigwood"]
+)		
+# Filter low count OTUs
+list_dds <- lapply(list_dds,function(dds)  dds[rowSums(counts(dds, normalize=T))>0,])
+
+# add full model to dds object
+list_dds <- lapply(list_dds,function(dds) {
+	design(dds) <- design;
+	dds <- dds[,dds$condition!="Sandra"];
+	colData(dds) <- droplevels(colData(dds));
+	dds}
+)
+
+# calculate fit
+list_dds2 <- lapply(list_dds,DESeq,parallel=T)
+list_dds >- list_dds2[1;4]
+		   
+# results COD vs Healthy
+res <- lapply(list_dds,results,alpha=alpha,parallel=T,contrast=c("condition","COD","Healthy"))
+res.merge <- lapply(res,function(res)data.table(inner_join(data.table(OTU=rownames(res),as.data.frame(res)),data.table(OTU=rownames(taxData),taxData))))
+sapply(names(res.merge),function(x) write.table(res.merge[[x]],paste(RHB,x,"COD_diff.txt",sep="_"),quote=F,sep="\t",na="",row.names=F))
+rm1 <- res.merge
+       
+# results AOD vs Healthy
+res <- lapply(list_dds,results,alpha=alpha,parallel=T,contrast=c("condition","AOD","Healthy"))
+res.merge <- lapply(res,function(res)data.table(inner_join(data.table(OTU=rownames(res),as.data.frame(res)),data.table(OTU=rownames(taxData),taxData))))
+sapply(names(res.merge),function(x) write.table(res.merge[[x]],paste(RHB,x,"AOD_diff.txt",sep="_"),quote=F,sep="\t",na="",row.names=F))
+rm2 <- res.merge
+
+# results Remission vs Healthy
+res <- lapply(list_dds,results,alpha=alpha,parallel=T,contrast=c("condition","Remission","Healthy"))
+res.merge <- lapply(res,function(res)data.table(inner_join(data.table(OTU=rownames(res),as.data.frame(res)),data.table(OTU=rownames(taxData),taxData))))
+sapply(names(res.merge),function(x) write.table(res.merge[[x]],paste(RHB,x,"Rem_diff.txt",sep="_"),quote=F,sep="\t",na="",row.names=F))
+rm3 <- res.merge
+
+# results COD vs AOD
+res <- lapply(list_dds,results,alpha=alpha,parallel=T,contrast=c("condition","COD","AOD"))
+res.merge <- lapply(res,function(res)data.table(inner_join(data.table(OTU=rownames(res),as.data.frame(res)),data.table(OTU=rownames(taxData),taxData))))
+sapply(names(res.merge),function(x) write.table(res.merge[[x]],paste(RHB,x,"COD_AOD_diff.txt",sep="_"),quote=F,sep="\t",na="",row.names=F))
+rm4 <- res.merge
+
+# results COD vs Remission
+res <- lapply(list_dds,results,alpha=alpha,parallel=T,contrast=c("condition","COD","Remission"))
+res.merge <- lapply(res,function(res)data.table(inner_join(data.table(OTU=rownames(res),as.data.frame(res)),data.table(OTU=rownames(taxData),taxData))))
+sapply(names(res.merge),function(x) write.table(res.merge[[x]],paste(RHB,x,"COD_REM_diff.txt",sep="_"),quote=F,sep="\t",na="",row.names=F))
+rm5 <- res.merge
+
+# results AOD vs Remission
+res <- lapply(list_dds,results,alpha=alpha,parallel=T,contrast=c("condition","AOD","Remission"))
+res.merge <- lapply(res,function(res)data.table(inner_join(data.table(OTU=rownames(res),as.data.frame(res)),data.table(OTU=rownames(taxData),taxData))))
+sapply(names(res.merge),function(x) write.table(res.merge[[x]],paste(RHB,x,"AOD_REM_diff.txt",sep="_"),quote=F,sep="\t",na="",row.names=F))
+rm6 <- res.merge
+     
+       
+all.tabs <- lapply(seq(1,4),function(i) data.table(list(
+	rm1[[i]] %>% select(-lfcSE,-stat,-pvalue) %>% rename(FC_COD = log2FoldChange)  %>% rename(padj_COD = padj),
+	rm2[[i]] %>% select(-lfcSE,-stat,-pvalue) %>% rename(FC_AOD = log2FoldChange)  %>% rename(padj_AOD = padj),
+	rm3[[i]] %>% select(-lfcSE,-stat,-pvalue) %>% rename(FC_REM = log2FoldChange)  %>% rename(padj_REM = padj),
+	rm4[[i]] %>% select(-lfcSE,-stat,-pvalue) %>% rename(FC_CA  = log2FoldChange)  %>% rename(padj_CA  = padj),
+	rm5[[i]] %>% select(-lfcSE,-stat,-pvalue) %>% rename(FC_CR  = log2FoldChange)  %>% rename(padj_CR  = padj),
+	rm6[[i]] %>% select(-lfcSE,-stat,-pvalue) %>% rename(FC_AR  = log2FoldChange)  %>% rename(padj_AR  = padj)
+        ) %>% Reduce(function(dt1,dt2) full_join(dt1,dt2), .))
+)	
+sapply(seq(1,4),function(i) write.table(all.tabs[[i]],paste(RHB,names(res.merge)[i],"diffs.txt",sep="_"),quote=F,sep="\t",na="",row.names=F))
+       
+# chestnuts and bigwood
+res <- lapply(list_dds[5:6],results,alpha=alpha)
+res.merge <- lapply(res,function(res)data.table(inner_join(data.table(OTU=rownames(res),as.data.frame(res)),data.table(OTU=rownames(taxData),taxData))))
+sapply(names(res.merge),function(x) write.table(res.merge[[x]],paste(RHB,x,"COD_diff.txt",sep="_"),quote=F,sep="\t",na="",row.names=F))
+       
+# output sig fasta
+writeXStringSet(readDNAStringSet(paste0(RHB,".otus.fa"))[ res.merge[padj<=0.05]$OTU],paste0(RHB,".sig.fa")) 
+
+# Merge
+
+rm <- lapply(rm2,function(rm) rm[,c(1,2,3,7),drop=F])      
+suffixes <- paste0("_",names(rm))
+test <- merge(merge(rm[[1]],rm[[2]],by="OTU",all=T,suffixes=suffixes[1:2]),
+	     merge(rm[[3]],rm[[4]],by="OTU",all=T,suffixes=suffixes[3:4]),
+	     all=T )
+res.merge <- data.table(inner_join(test,data.table(OTU=rownames(taxData),taxData)))
+fwrite(res.merge,"AOD.diff.txt",sep="\t",quote=F,na="") 
