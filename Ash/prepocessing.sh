b2c9511f1e7641494c5363a65e4fbed9b1dc5fb8
Greg Deakin <GregDeakin@users.noreply.github.com>
2019-10-30 09:42:56 +0000

    Update prepocessing.sh

--- Ash/prepocessing.sh
+++ Ash/prepocessing.sh
@@ -1,1 +1,23 @@
- 
+# make project folders
+PROJECT_FOLDER=~/projects/Ash
+mkdir -p $PROJECT_FOLDER
+ln -s $MBPL $PROJECT_FOLDER/metabarcoding_pipeline 
+
+RUN=.
+mkdir -p $PROJECT_FOLDER/data/$RUN/fastq
+mkdir $PROJECT_FOLDER/data/$RUN/quality
+mkdir $PROJECT_FOLDER/data/$RUN/ambiguous
+mkdir $PROJECT_FOLDER/data/$RUN/cluster
+
+for s in BAC FUN; do
+  mkdir -p $PROJECT_FOLDER/data/$RUN/$s/fastq
+  mkdir $PROJECT_FOLDER/data/$RUN/$s/filtered
+  mkdir $PROJECT_FOLDER/data/$RUN/$s/unfiltered
+  mkdir $PROJECT_FOLDER/data/$RUN/$s/fasta
+done
+
+# quality check
+for FILE in $PROJECT_FOLDER/data/$RUN/fastq/*; do 
+  $PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c qcheck $FILE $PROJECT_FOLDER/data/$RUN/quality
+done
+
