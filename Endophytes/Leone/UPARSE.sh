355859de6298ed1749d5cef02fb50f05d2dffc24
Greg Deakin <GregDeakin@users.noreply.github.com>
2019-11-07 10:24:22 +0000

    Update UPARSE.sh

--- Endophytes/Leone/UPARSE.sh
+++ Endophytes/Leone/UPARSE.sh
@@ -5,15 +5,15 @@ PROJECT_FOLDER=~/projects/Endophytes
 RUN=leone_230919
 
 # FUN
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c UPARSE $PROJECT_FOLDER $RUN FUN 0 0
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER $RUN FUN 0 0
 sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/FUN.zotus.fa  # workaround for uparse bug
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN FUN
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN FUN 
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c OTU $PROJECT_FOLDER $RUN FUN 23 21
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN FUN
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN FUN 
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTUS $PROJECT_FOLDER $RUN FUN 22 21
 
 # BAC
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c UPARSE $PROJECT_FOLDER $RUN BAC 0 0
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER $RUN BAC 0 0
 sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/BAC.zotus.fa
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN BAC
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN BAC
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c OTU $PROJECT_FOLDER $RUN BAC 17 21
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN BAC
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN BAC
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTUS $PROJECT_FOLDER $RUN BAC 17 21
