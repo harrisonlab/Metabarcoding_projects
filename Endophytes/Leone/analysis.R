e97152ca2b856cd48fdc9588dd2f78985d5eba9b
Greg Deakin <GregDeakin@users.noreply.github.com>
2019-11-07 10:55:37 +0000

    Update analysis.R

--- Endophytes/Leone/analysis.R
+++ Endophytes/Leone/analysis.R
@@ -3,60 +3,213 @@
 #===============================================================================
 
 library(DESeq2)
-library(BiocParallel)
 library(data.table)
-library(plyr)
 library(tidyverse)
-library(metafuncs)
+library(Biostrings)
 library(vegan)
 library(lmPerm)
+library(phyloseq)
+library(metacoder)
+library(ape)
+library(metafuncs)
+library(BiocParallel)
 
 #environment(plot_ordination) <- environment(ordinate) <- environment(plot_richness) <- environment(phyloseq::ordinate)
 #assignInNamespace("plot_ordination",value=plot_ordination,ns="phyloseq")
 
 #===============================================================================
+#       Functions
+#===============================================================================
+
+# adapted version of phyloTaxaTidy to mash names by conf
+phyloTidy <- function(taxData,conf,setCount=T) {
+  X <- taxData[,1:7]
+  Y <- taxData[,9:15]
+  Y[,1] <-1.00
+  X[apply(Y,2,function(y)y<conf)] <- "unknown"
+  n <- c("kingdom","phylum","class","order","family","genus","species")
+  X <-apply(X,2,function(x) {
+    x <- sub("\\(.*","",x)
+    x <- sub("_SH[0-9]+\\..*","",x)
+    x <- gsub("_"," ",x)
+    #x <- sub("_"," ",x)
+    x
+  })
+  
+  td <- as.data.table(X,keep.rownames = "OTU")
+  cols <- names(td)[-1]
+  num <- td[,.N,by=cols]
+  num2 <- invisible(apply(num,1,function(x) as.data.table(t(matrix(x[-8],nrow=7,ncol=as.numeric(x[8]))))))
+  invisible(lapply(num2,setnames,cols))
+  invisible(lapply(num2,function(x)x[,counts:=1:nrow(x)]))
+  td2 <- do.call("rbind",num2)
+  td[,(cols):=lapply(.SD,as.character),.SDcols=cols]
+  setorderv(td, cols[-1])
+  setorderv(td2,cols[-1])
+  
+  td$counts <- td2$counts
+  td[,(cols):=lapply(.SD,as.factor),.SDcols=cols]
+  taxData <- as.data.frame(td)
+  rownames(taxData) <- taxData$OTU
+  taxData <- taxData[,-1]
+  if(setCount) {
+    taxData$species <- as.factor(paste(taxData$species,taxData$counts,sep=" "))
+    taxData <- taxData[,-8]
+  }
+  taxData
+}
+
+funq <- function(lfc,p){lfc[c(which(p>0.1),which(is.na(p)))] <- 0;lfc}
+
+qf <- function(x)sub("Fungi;unknown.*","REMOVE",x)
+
+# get the full taxonomy for each taxon_id
+# get_full_taxa <- function(taxon_id,obj) {
+#   t_var   <- c(obj$taxon_indexes()[[taxon_id]],obj$supertaxa()[[taxon_id]])
+#   t_names <- sapply(t_var,function(x) {obj$taxa[[x]]$get_name()})
+#   paste(rev(t_names),collapse = ";")
+# }
+
+get_full_taxa <- function(taxon_id,t1=t1,t2=t2) {
+  t_names <- t2[c(taxon_id,unlist(t1[taxon_id]))]
+  paste(rev(t_names),collapse = ";")
+}
+
+MCDESeq <- function (OBJ,formula,Rank,colData) {
+  
+  o <- OBJ$clone(deep=T)
+  formula <- formula(formula)
+  # set rank for collapsing
+  #  Rank = "genus"
+  # collapse OTU abundances at each taxonomic rank at and above Rank 
+  o$data$tax_abund <- o %>% 
+    taxa::filter_taxa(taxon_ranks == Rank,supertaxa=T) %>%
+    calc_taxon_abund(data = "otu_table",cols = colData$sample_id)
+  
+  # metacoder differential analysis - this is bobbins  
+  #FUN_ENDO$data$diff_table <- compare_groups(FUN_ENDO, dataset = "otu_table",
+  #                                           cols = colData$sample_id, # What columns of sample data to use
+  #                                           groups = colData$status) # What category each sample is assigned to
+  
+  # get the otu table with taxon_ids  - rejig to get taxon_id as rownames
+  
+  
+  otu_table <- as.data.table(o$data$otu_table) #metacoder:::get_taxmap_table(OBJ, "otu_table")
+  # merge duplicate "species"
+  numeric_cols <- which(sapply(otu_table, is.integer))
+  otu_table <- as.data.frame(otu_table[, lapply(.SD, sum), by = taxon_id, .SDcols = numeric_cols])
+  rownames(otu_table) <- otu_table[,1]
+  otu_table <- otu_table[,c(-1)]
+  
+  
+  # get Rank and above abundances - rejig to get taxon_id as rownames
+  tax_abund <- as.data.frame(o$data$tax_abund) #metacoder:::get_taxmap_table(OBJ, "tax_abund")
+  rownames(tax_abund) <- tax_abund[,1]
+  tax_abund <- tax_abund[,c(-1)]
+  
+  # set character columns to factors
+  colData <- as.data.frame(unclass(as.data.frame(colData)))
+  
+  # create new dds object from combined tables
+  dds <- DESeqDataSetFromMatrix(rbind(otu_table,tax_abund),colData,~1)
+  #cols <- colnames(colData(dds))[sapply(colData(dds),is.factor)]
+  #dds$status <- as.factor(dds$status)
+  design(dds) <- formula#~status
+  dds <- DESeq(dds)
+  return(dds)
+  # res <- results(dds,alpha=alpha,contrast=contrast)
+  # 
+  # # make a data table from the resuults
+  # res_merge <- as.data.table(as.data.frame(res),keep.rownames="taxon_id")
+  # 
+  # # add second log fold change column with fold changes for non sig taxa set to 0 (for colouring purposes on graphs)
+  # res_merge[,log2FoldChange2:=funq(log2FoldChange,padj)]
+  # 
+  # # add full taxonomy to results (this is slow - good reason not to use S4 object model!)
+  # 
+  # # order the results
+  # setkey(res_merge,taxon_id)
+  # 
+  # # these are required due to the restrictive interace of taxa/metacoder 
+  # # reduces the time required to get the full taxonomy of each taxon id by about x10,000
+  # t1 <- o$supertaxa()
+  # t2 <- o$taxon_names()
+  # res_merge[,taxonomy:=sapply(1:nrow(res_merge),get_full_taxa,t1,t2)]
+  # 
+  # # add results as diff_table (could be called anything - but for consistency with metacoder keep same names)
+  # #
+  # as_tibble(res_merge)
+}
+
+MCDESres <- function (OBJ,dds,Rank,colData,contrast,alpha=0.1) {
+  
+  o <- OBJ$clone(deep=T)
+  # set rank for collapsing
+  #  Rank = "genus"
+  # collapse OTU abundances at each taxonomic rank at and above Rank 
+  o$data$tax_abund <- o %>% 
+    taxa::filter_taxa(taxon_ranks == Rank,supertaxa=T) %>%
+    calc_taxon_abund(data = "otu_table",cols = colData$sample_id)
+  
+  # metacoder differential analysis - this is bobbins  
+  #FUN_ENDO$data$diff_table <- compare_groups(FUN_ENDO, dataset = "otu_table",
+  #                                           cols = colData$sample_id, # What columns of sample data to use
+  #                                           groups = colData$status) # What category each sample is assigned to
+  
+  # get the otu table with taxon_ids  - rejig to get taxon_id as rownames
+  otu_table <- as.data.table(o$data$otu_table) #metacoder:::get_taxmap_table(OBJ, "otu_table")
+  # merge duplicate "species"
+  numeric_cols <- which(sapply(otu_table, is.integer))
+  otu_table <- as.data.frame(otu_table[, lapply(.SD, sum), by = taxon_id, .SDcols = numeric_cols])
+  rownames(otu_table) <- otu_table[,1]
+  otu_table <- otu_table[,c(-1,-2)]
+  
+  # get Rank and above abundances - rejig to get taxon_id as rownames
+  tax_abund <- as.data.frame(o$data$tax_abund) #metacoder:::get_taxmap_table(OBJ, "tax_abund")
+  rownames(tax_abund) <- tax_abund[,1]
+  tax_abund <- tax_abund[,c(-1)]
+  
+  # set character columns to factors
+  colData <- as.data.frame(unclass(as.data.frame(colData)))
+  
+  # create new dds object from combined tables
+  #dds <- DESeqDataSetFromMatrix(rbind(otu_table,tax_abund),colData,~1)
+  #cols <- colnames(colData(dds))[sapply(colData(dds),is.factor)]
+  #dds$status <- as.factor(dds$status)
+  #design(dds) <- formula#~status
+  #dds <- DESeq(dds)
+  res <- results(dds,alpha=alpha,contrast=contrast)
+  
+  # make a data table from the resuults
+  res_merge <- as.data.table(as.data.frame(res),keep.rownames="taxon_id")
+  
+  # add second log fold change column with fold changes for non sig taxa set to 0 (for colouring purposes on graphs)
+  res_merge[,log2FoldChange2:=funq(log2FoldChange,padj)]
+  
+  # add full taxonomy to results (this is slow - good reason not to use S4 object model!)
+  
+  # order the results
+  setkey(res_merge,taxon_id)
+  
+  # these are required due to the restrictive interace of taxa/metacoder 
+  # reduces the time required to get the full taxonomy of each taxon id by about x10,000
+  t1 <- o$supertaxa()
+  t2 <- o$taxon_names()
+  res_merge[,taxonomy:=sapply(1:nrow(res_merge),get_full_taxa,t1,t2)]
+  
+  # add results as diff_table (could be called anything - but for consistency with metacoder keep same names)
+  #
+  as_tibble(res_merge)
+}
+
+#===============================================================================
 #       Load data
 #===============================================================================
 
-# load otu count table
-countData <- read.table("BAC.otus_table.txt",header=T,sep="\t",row.names=1, comment.char = "")
-
-# load sample metadata
-colData <- read.table("colData",header=T,sep="\t",row.names=1)
-
-# load taxonomy data
-taxData <- read.table("BAC.taxa",header=F,sep=",",row.names=1)
-
-# reorder columns
-taxData<-taxData[,c(1,3,5,7,9,11,13,2,4,6,8,10,12,14)]
-
-# add best "rank" at 0.65 confidence and tidy-up the table
-#taxData<-phyloTaxaTidy(taxData,0.65)
-
-# get unifrac dist
-#phylipData <- fread.phylip("BAC.phy")
-
-#njtree <- nj(as.dist(phylipData))
-
-# save data into a list
-ubiom_BAC <- list(
-  countData=countData,
-  colData=colData,
-  taxData=taxData,
- # phylipData=phylipData,
-  #njtree=njtree,
-  RHB="BAC"
-)
-
-
-# Fungi all in one call
-ubiom_FUN <- list(
-  countData=read.table("FUN.otus_table.txt",header=T,sep="\t",row.names=1,comment.char = ""),
-  colData=read.table("colData",header=T,sep="\t",row.names=1),
-  taxData=phyloTaxaTidy(read.table("FUN.taxa",header=F,sep=",",row.names=1)[,c(1,3,5,7,9,11,13,2,4,6,8,10,12,14)],0.65),
-  RHB="FUN"
-)
-
+ubiom_BAC <- loadData("BAC.otus_table.txt","colData","BAC.taxa",RHB="BAC")
+# ubiom_BAC$countData <- ubiom_BAC$countData[,colnames(ubiom_BAC$countData)%in%rownames(ubiom_BAC$colData)]
+ubiom_FUN <- loadData("FUN.otus_table.txt","colData","FUN.taxa",RHB="FUN")
+# ubiom_FUN$countData <- ubiom_FUN$countData[,colnames(ubiom_FUN$countData)%in%rownames(ubiom_FUN$colData)]
 
 #===============================================================================
 #       Combine species
@@ -66,37 +219,28 @@ ubiom_FUN <- list(
 
 # Fungi
 invisible(mapply(assign, names(ubiom_FUN), ubiom_FUN, MoreArgs=list(envir = globalenv())))
-combinedTaxa <- combineTaxa("FUN.taxa",column_order=c(1:7,9:15))
+combinedTaxa <- combineTaxa("FUN.taxa")
 countData <- combCounts(combinedTaxa,countData)
 taxData <- combTaxa(combinedTaxa,taxData)
 ubiom_FUN$countData <- countData
 ubiom_FUN$taxData <- taxData
 
 #===============================================================================
+#       Create DEseq objects
+#===============================================================================
+		   
+ubiom_FUN$dds <- ubiom_to_des(ubiom_FUN,filter=expression(colSums(countData)>=1000&colData$Block!="R"))
+ubiom_BAC$dds <- ubiom_to_des(ubiom_BAC,filter=expression(colSums(countData)>=1000&colData$Block!="R"))
+# ubiom_OO$dds <- ubiom_to_des(ubiom_OO,filter=expression(colSums(countData)>=1000&colData$Block!="R"),calcFactors=geoMeans)
+# ubiom_NEM$dds <- ubiom_to_des(ubiom_NEM,filter=expression(colSums(countData)>=500&colData$Block!="R"))
+
+#===============================================================================
 #      ****FUNGI/BACTERIA****
 #===============================================================================
-
 # Fungi
 invisible(mapply(assign, names(ubiom_FUN), ubiom_FUN, MoreArgs=list(envir = globalenv())))
 # Bacteria
 invisible(mapply(assign, names(ubiom_BAC), ubiom_BAC, MoreArgs=list(envir = globalenv())))
-
-#===============================================================================
-#       Create DEseq objects
-#===============================================================================
-
-colData$parent <- as.factor(colData$parent)
-
-# ensure colData rows and countData columns have the same order
-colData <- colData[names(countData),]
-
-design<-~1
-
-#create DES object
-# colnames(countData) <- row.names(colData)
-dds<-DESeqDataSetFromMatrix(countData,colData,design)
-
-sizeFactors(dds) <-sizeFactors(estimateSizeFactors(dds))
 
 #===============================================================================
 #       Sample rarefaction plots
@@ -161,13 +305,9 @@ invisible(mapply(assign, names(ubiom_BAC), ubiom_BAC, MoreArgs=list(envir = glob
 g1 <- gfunc(as.data.frame(counts(dds)),as.data.frame(colData(dds)),"Bacteria")
 invisible(mapply(assign, names(ubiom_FUN), ubiom_FUN, MoreArgs=list(envir = globalenv())))
 g2 <- gfunc(as.data.frame(counts(dds)),as.data.frame(colData(dds)),"Fungi")
-invisible(mapply(assign, names(ubiom_OO), ubiom_OO, MoreArgs=list(envir = globalenv())))
-g3 <- gfunc(as.data.frame(counts(dds)),as.data.frame(colData(dds)),"Oomycetes")
-invisible(mapply(assign, names(ubiom_NEM), ubiom_NEM, MoreArgs=list(envir = globalenv())))
-g4 <- gfunc(as.data.frame(counts(dds)),as.data.frame(colData(dds)),"Nematodes")
 
-glegend <- get_legend(g)
-ggsave("rarefaction_all.pdf",grid.arrange(g1,g2,g3,g4,left=textGrob(label=expression("Log"[10] * " aligned sequenecs"),rot=90),bottom="OTU count",nrow=2))                              
+glegend <- get_legend(g) # this won't work
+ggsave("rarefaction_all.pdf",grid.arrange(g1,g2,left=textGrob(label=expression("Log"[10] * " aligned sequenecs"),rot=90),bottom="OTU count",nrow=2))                              
 
 
 #===============================================================================
@@ -177,6 +317,8 @@ ggsave("rarefaction_all.pdf",grid.arrange(g1,g2,g3,g4,left=textGrob(label=expres
 # Recreate dds object and don't filter for low counts before running Alpha diversity
 
 # plot alpha diversity - plot_alpha will convert normalised abundances to integer values
+plot_alpha(counts(dds,normalize=T),colData(dds),design="Treatment",colour=NULL,measures=c("Chao1", "Shannon", "Simpson","Observed"),type="box")		   
+		   
 ggsave(paste(RHB,"Alpha.pdf",sep="_"),plot_alpha(counts(dds,normalize=T),colData(dds),design="Treatment",colour=NULL,measures=c("Chao1", "Shannon", "Simpson","Observed")))
 ggsave(paste(RHB,"Alpha_Chao1.pdf",sep="_"),plot_alpha(counts(dds,normalize=T),colData(dds),design="Treatment",colour="Genotype",measures=c("Chao1"))) # ,limits=c(0,xxx,"Chao1")
 ggsave(paste(RHB,"Alpha_Shannon.pdf",sep="_"),plot_alpha(counts(dds,normalize=T),colData(dds),design="Treatment",colour="Genotype",measures=c("Shannon")))
