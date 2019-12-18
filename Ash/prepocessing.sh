b2320915ef256db47e2153a37af911f76bda218b
Greg Deakin <GregDeakin@users.noreply.github.com>
2019-12-18 09:34:35 +0000

    Update prepocessing.sh

--- Ash/prepocessing.sh
+++ Ash/prepocessing.sh
@@ -21,3 +21,17 @@ for FILE in $PROJECT_FOLDER/data/$RUN/fastq/*; do
   $PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c qcheck $FILE $PROJECT_FOLDER/data/$RUN/quality
 done
 
+# Demultiplex bacterial and fungal amplicons
+P1F=CCTACGGGNGGCWGCAG
+P1R=GACTACHVGGGTATCTAATCC
+P2F=CTTGGTCATTTAGAGGAAGTAA
+P2R=ATATGCTTAAGTTCAGCGGG
+
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c demultiplex \
+ "$PROJECT_FOLDER/data/$RUN/fastq/*_R1_*" 0 \
+ $P1F $P1R $P2F $P2R
+
+mv $PROJECT_FOLDER/data/$RUN/fastq/*ps1* $PROJECT_FOLDER/data/$RUN/BAC/fastq/.
+mv $PROJECT_FOLDER/data/$RUN/fastq/*ps2* $PROJECT_FOLDER/data/$RUN/FUN/fastq/.
+mv $PROJECT_FOLDER/data/$RUN/fastq/*ambig* $PROJECT_FOLDER/data/$RUN/ambiguous/.
+
