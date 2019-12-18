8dfdfb7ddf47401bc8d4159338d1096e2b78f166
Greg Deakin <GregDeakin@users.noreply.github.com>
2019-12-18 09:42:22 +0000

    Update prepocessing.sh

--- Ash/prepocessing.sh
+++ Ash/prepocessing.sh
@@ -35,3 +35,24 @@ mv $PROJECT_FOLDER/data/$RUN/fastq/*ps1* $PROJECT_FOLDER/data/$RUN/BAC/fastq/.
 mv $PROJECT_FOLDER/data/$RUN/fastq/*ps2* $PROJECT_FOLDER/data/$RUN/FUN/fastq/.
 mv $PROJECT_FOLDER/data/$RUN/fastq/*ambig* $PROJECT_FOLDER/data/$RUN/ambiguous/.
 
+# pre-process BAC files (min length 300, max diffs 5, quality 0.5)
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c 16Spre \
+ "$PROJECT_FOLDER/data/$RUN/BAC/fastq/*R1*.fastq" \
+ $PROJECT_FOLDER/data/$RUN/BAC \
+ $PROJECT_FOLDER/metabarcoding_pipeline/primers/adapters.db \
+ 300 5 0.5 17 21
+
+# Pre-process FUN files (min length 150, quality 1, truncate final 5 bases) 
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c ITSpre \
+ "$PROJECT_FOLDER/data/$RUN/FUN/fastq/*R1*.fastq" \
+ $PROJECT_FOLDER/data/$RUN/FUN \
+ $PROJECT_FOLDER/metabarcoding_pipeline/primers/primers.db \
+ 150 1 22 21 5
+ 
+# move fasta files 
+for F in $PROJECT_FOLDER/data/$RUN/FUN/fasta/*_R1.fa; do 
+ FO=$(awk -F"/" '{print $NF}' <<< $F|awk -F"_" '{print $1".r1.fa"}'); 
+ L=$(awk -F"/" '{print $NF}' <<< $F|awk -F"_" '{print $1}') ;
+ echo $L
+ awk -v L=$L '/>/{sub(".*",">"L"."(++i))}1' $F > $FO.tmp && mv $FO.tmp $PROJECT_FOLDER/data/$RUN/FUN/filtered/$FO;
+done   
