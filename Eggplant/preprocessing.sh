87e08a8e23cdca202625c51b86fa11612cf6f1b9
Greg Deakin <GregDeakin@users.noreply.github.com>
2019-12-19 10:13:42 +0000

    Create preprocessing.sh

---
+++ Eggplant/preprocessing.sh
@@ -0,0 +1,58 @@
+# make project folders
+PROJECT_FOLDER=~/projects/Eggplant
+mkdir -p $PROJECT_FOLDER
+ln -s $MBPL $PROJECT_FOLDER/metabarcoding_pipeline 
+
+RUN=.
+mkdir -p $PROJECT_FOLDER/data/$RUN/fastq
+mkdir $PROJECT_FOLDER/data/$RUN/quality
+mkdir $PROJECT_FOLDER/data/$RUN/ambiguous
+mkdir $PROJECT_FOLDER/data/$RUN/cluster
+
+for s in BAC FUN; do
+  mkdir -p $PROJECT_FOLDER/data/$RUN/$s/fastq
+  mkdir $PROJECT_FOLDER/data/$RUN/$s/filtered
+  mkdir $PROJECT_FOLDER/data/$RUN/$s/unfiltered
+  mkdir $PROJECT_FOLDER/data/$RUN/$s/fasta
+done
+
+# quality check
+for FILE in $PROJECT_FOLDER/data/$RUN/fastq/*; do 
+  $PROJECT_FOLDER/metabarcoding_pipeline/scripts/slurm/PIPELINE.sh -c qcheck $FILE $PROJECT_FOLDER/data/$RUN/quality
+done
+
+# Demultiplex bacterial and fungal amplicons
+P1F=CCTACGGGNGGCWGCAG
+P1R=GACTACHVGGGTATCTAATCC
+P2F=CTTGGTCATTTAGAGGAAGTAA
+P2R=ATATGCTTAAGTTCAGCGGG
+
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c demultiplex \
+ "$PROJECT_FOLDER/data/$RUN/fastq/*_R1_*" 0 \
+ $P1F $P1R $P2F $P2R
+
+mv $PROJECT_FOLDER/data/$RUN/fastq/*ps1* $PROJECT_FOLDER/data/$RUN/BAC/fastq/.
+mv $PROJECT_FOLDER/data/$RUN/fastq/*ps2* $PROJECT_FOLDER/data/$RUN/FUN/fastq/.
+mv $PROJECT_FOLDER/data/$RUN/fastq/*ambig* $PROJECT_FOLDER/data/$RUN/ambiguous/.
+
+# pre-process BAC files (min length 300, max diffs 5, quality 0.5)
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c 16Spre \
+ "$PROJECT_FOLDER/data/$RUN/BAC/fastq/*R1*.fastq" \
+ $PROJECT_FOLDER/data/$RUN/BAC \
+ $PROJECT_FOLDER/metabarcoding_pipeline/primers/adapters.db \
+ 300 5 0.5 17 21
+
+# Pre-process FUN files (min length 150, quality 1, truncate final 5 bases) 
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c ITSpre \
+ "$PROJECT_FOLDER/data/$RUN/FUN/fastq/*R1*.fastq" \
+ $PROJECT_FOLDER/data/$RUN/FUN \
+ $PROJECT_FOLDER/metabarcoding_pipeline/primers/primers.db \
+ 150 1 22 21 5
+ 
+# move fasta files 
+for F in $PROJECT_FOLDER/data/$RUN/FUN/fasta/*_R1.fa; do 
+ FO=$(awk -F"/" '{print $NF}' <<< $F|awk -F"_" '{print $1".r1.fa"}'); 
+ L=$(awk -F"/" '{print $NF}' <<< $F|awk -F"_" '{print $1}') ;
+ echo $L
+ awk -v L=$L '/>/{sub(".*",">"L"."(++i))}1' $F > $FO.tmp && mv $FO.tmp $PROJECT_FOLDER/data/$RUN/FUN/filtered/$FO;
+done   
