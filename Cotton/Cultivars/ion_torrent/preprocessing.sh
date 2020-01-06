9ab341307f87d7c641934860b6f3ec3c68e00e31
Greg Deakin <GregDeakin@users.noreply.github.com>
2020-01-06 12:28:09 +0000

    Update preprocessing.sh

--- Cotton/Cultivars/ion_torrent/preprocessing.sh
+++ Cotton/Cultivars/ion_torrent/preprocessing.sh
@@ -12,6 +12,22 @@ for s in BAC FUN; do
 done
 
 
+# QC
+for FILE in $PROJECT_FOLDER/data/$RUN/BAC/fastq/*; do
+ $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c qcheck $FILE $PROJECT_FOLDER/data/$RUN/BAC/quality
+done
+
+for FILE in $PROJECT_FOLDER/data/$RUN/FUN/fastq/*; do
+ $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c qcheck $FILE $PROJECT_FOLDER/data/$RUN/FUN/quality
+done
+
+# Length trimming
+cat $PROJECT_FOLDER/$RUN/BAC/fastq/* > bac.cat.fq
+cat $PROJECT_FOLDER/$RUN/FUN/fastq/* > fun.cat.fq
+
+## ion torrent S5 looks like it uses extra phred 33 charcters (L and M - maybe more?) below to check
+awk 'NR % 4 ==0' LM28.D10.fastq|tr -d '\n'|grep -o . |sort -u|paste -s -d '\0'
+
 #### BELOW TO BE UPDATED ####
 
 #bacteria
