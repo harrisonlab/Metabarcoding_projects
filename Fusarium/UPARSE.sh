487cdbc0d71ec1d4aaf203db5e89a35b2ebfab76
Greg Deakin <GregDeakin@users.noreply.github.com>
2020-02-17 18:40:57 +0000

    Update UPARSE.sh

--- Fusarium/UPARSE.sh
+++ Fusarium/UPARSE.sh
@@ -7,18 +7,20 @@ $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FO
 sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/OG1.zotus.fa  # workaround for uparse bug
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN OG1
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN OG1 
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTUS $PROJECT_FOLDER $RUN OG1 22 21 OG1.otus.fa
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTUS $PROJECT_FOLDER $RUN OG1 25 23 OG1.otus.fa
 
 # OG4
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER $RUN OG4 0 0
 sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/OG4.zotus.fa
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN OG4
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN OG4
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTUS $PROJECT_FOLDER $RUN OG4 17 21 OG4.otus.fa
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTUS $PROJECT_FOLDER $RUN OG4 23 22 OG4.otus.fa
 
 # TEF
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER $RUN TEF 0 0
 sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/TEF.zotus.fa
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN TEF
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN TEF
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTUS $PROJECT_FOLDER $RUN TEF 17 21 TEF.otus.fa
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTUS $PROJECT_FOLDER $RUN TEF 22 21 TEF.otus.fa
+
+
