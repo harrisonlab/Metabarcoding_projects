38c09c6ba85b6707c1a30493b75ac9ebfe6f3f72
Greg Deakin <GregDeakin@users.noreply.github.com>
2020-02-17 18:09:01 +0000

    Update prepocessing.sh

--- Fusarium/prepocessing.sh
+++ Fusarium/prepocessing.sh
@@ -9,7 +9,7 @@ mkdir $PROJECT_FOLDER/data/$RUN/quality
 mkdir $PROJECT_FOLDER/data/$RUN/ambiguous
 mkdir $PROJECT_FOLDER/data/$RUN/cluster
 
-for s in BAC FUN OG1 OG4; do
+for s in TEF OG1 OG4 SIX; do
   mkdir -p $PROJECT_FOLDER/data/$RUN/$s/fastq
   mkdir $PROJECT_FOLDER/data/$RUN/$s/filtered
   mkdir $PROJECT_FOLDER/data/$RUN/$s/unfiltered
@@ -34,8 +34,8 @@ P1R=CCGCTTATTGATATGCTTAARTTCAG
 #P2=TEF
 P2F=GGTCACTTGATCTACCAGTGCG
 P2R=CCCARGCGTACTTGAAGRAAC
-# SIX5
-#P3=SIX5
+# SIX
+#P3=SIX
 P3F=AATCATCCTCACGATTATGTGTG
 P3R=CTGATGGCAAAGGTCATAGAATGTT
 # T4 orthogroup 13890
@@ -58,24 +58,36 @@ $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c demultiplex \
  "$PROJECT_FOLDER/data/$RUN/fastq/*_R1_*" 1 \
  $P2F $P2R $P5F $P5R $P6F $P6R
 
-mv $PROJECT_FOLDER/data/$RUN/fastq/*ps1* $PROJECT_FOLDER/data/$RUN/BAC/fastq/.
-mv $PROJECT_FOLDER/data/$RUN/fastq/*ps2* $PROJECT_FOLDER/data/$RUN/FUN/fastq/.
+mv $PROJECT_FOLDER/data/$RUN/fastq/*ps1* $PROJECT_FOLDER/data/$RUN/TEF/fastq/.
+mv $PROJECT_FOLDER/data/$RUN/fastq/*ps2* $PROJECT_FOLDER/data/$RUN/SIX/fastq/.
+mv $PROJECT_FOLDER/data/$RUN/fastq/*ps3* $PROJECT_FOLDER/data/$RUN/OG1/fastq/.
+mv $PROJECT_FOLDER/data/$RUN/fastq/*ps4* $PROJECT_FOLDER/data/$RUN/OG4/fastq/.
+
 mv $PROJECT_FOLDER/data/$RUN/fastq/*ambig* $PROJECT_FOLDER/data/$RUN/ambiguous/.
 
-# pre-process BAC files (min length 300, max diffs 5, quality 0.5)
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c 16Spre \
- "$PROJECT_FOLDER/data/$RUN/BAC/fastq/*R1*.fastq" \
- $PROJECT_FOLDER/data/$RUN/BAC \
- $PROJECT_FOLDER/metabarcoding_pipeline/primers/adapters.db \
- 300 5 0.5 17 21 400
 
-# Pre-process FUN files (min length 150, quality 1, truncate final 5 bases) 
-$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c ITSpre \
- "$PROJECT_FOLDER/data/$RUN/FUN/fastq/*R1*.fastq" \
- $PROJECT_FOLDER/data/$RUN/FUN \
- $PROJECT_FOLDER/metabarcoding_pipeline/primers/primers.db \
- 150 1 22 21 5
+# pre-process OG1 files (min length 300, max diffs 10 (30), quality 0.5,min merged length 356)
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c 16Spre \
+ "$PROJECT_FOLDER/data/$RUN/OG1/fastq/*R1*.fastq" \
+ $PROJECT_FOLDER/data/$RUN/OG1 \
+ $PROJECT_FOLDER/metabarcoding_pipeline/primers/adapters.db \
+ 300 10 0.5 25 23 356
+
+# pre-process OG4 files (min length 250, max diffs 12 (30), quality 0.5,min merged length 356)
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c 16Spre \
+ "$PROJECT_FOLDER/data/$RUN/OG4/fastq/*R1*.fastq" \
+ $PROJECT_FOLDER/data/$RUN/OG4 \
+ $PROJECT_FOLDER/metabarcoding_pipeline/primers/adapters.db \
+ 250 12 0.5 23 22 277
+
+# pre-process TEF files (min length 250, max diffs 12 (30), quality 0.5,min merged length 356)
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c 16Spre \
+ "$PROJECT_FOLDER/data/$RUN/OG4/fastq/*R1*.fastq" \
+ $PROJECT_FOLDER/data/$RUN/OG4 \
+ $PROJECT_FOLDER/metabarcoding_pipeline/primers/adapters.db \
+ 250 12 0.5 23 22 320
  
+
 # move fasta files 
 for F in $PROJECT_FOLDER/data/$RUN/FUN/fasta/*_R1.fa; do 
  FO=$(awk -F"/" '{print $NF}' <<< $F|awk -F"_" '{print $1".r1.fa"}'); 
