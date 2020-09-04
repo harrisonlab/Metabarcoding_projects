e973ca5a90bb29f1a12fd5b36e4e61ebf5670d96
Greg Deakin <GregDeakin@users.noreply.github.com>
2020-09-04 13:06:36 +0100

    Update UPARSE.sh

--- Eggplant/UPARSE.sh
+++ Eggplant/UPARSE.sh
@@ -14,3 +14,40 @@ sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/BAC.zotus.fa
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN BAC
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN BAC
 $PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTUS $PROJECT_FOLDER $RUN BAC BAC.otus.fa 17 21 
+
+#### ADDITIONAL CODE ####
+
+# folder containing project files
+PROJECT_FOLDER=~/projects/Eggplant
+
+RUN=root_stem
+
+# FUN
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER $RUN FUN 0 0
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c UPARSE $PROJECT_FOLDER $RUN BAC 0 0
+
+sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/FUN.zotus.fa  # workaround for uparse bug
+#$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN FUN
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN FUN 
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTUS $PROJECT_FOLDER $RUN FUN FUN.otus.fa 22 21
+
+# BAC
+
+sed -i -e 's/Zotu/OTU/' $PROJECT_FOLDER/data/$RUN/BAC.zotus.fa
+#$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c dist $PROJECT_FOLDER $RUN BAC
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c tax_assign $PROJECT_FOLDER $RUN BAC
+$PROJECT_FOLDER/metabarcoding_pipeline/scripts/PIPELINE.sh -c OTUS $PROJECT_FOLDER $RUN BAC BAC.otus.fa 17 21 
+
+
+cp ../../data/root_stem/BAC.otu_table.txt .
+cp ../../data/root_stem/BAC.utax.taxa .
+cp ../../data/root_stem/BAC.sintax.taxa .
+cp ../../data/root_stem/BAC.otus.fa .
+
+cp ../../data/root_stem/FUN.otu_table.txt .
+cp ../../data/root_stem/FUN.utax.taxa .
+cp ../../data/root_stem/FUN.sintax.taxa .
+cp ../../data/root_stem/FUN.otus.fa .
+
+
+
